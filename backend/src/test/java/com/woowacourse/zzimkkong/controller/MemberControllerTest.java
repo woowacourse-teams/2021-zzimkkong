937e1a54fff439b51ecdb04ed89e4321ffc7cb8b
y_woo :) <dusdn1702@naver.com>
2021-07-08 21:03:17 +0900

    feat: restdocs를 이용한 api 문서화

--- backend/src/test/java/com/woowacourse/zzimkkong/controller/MemberControllerTest.java
+++ backend/src/test/java/com/woowacourse/zzimkkong/controller/MemberControllerTest.java
@@ -2,19 +2,39 @@ package com.woowacourse.zzimkkong.controller;
 
 import com.woowacourse.zzimkkong.dto.MemberSaveRequest;
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.documentationConfiguration;
 
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
+@AutoConfigureRestDocs
 class MemberControllerTest extends AcceptanceTest {
     public static final String EMAIL = "pobi@email.com";
     public static final String PASSWORD = "test1234";
     public static final String ORGANIZATION = "루터";
+
+    @BeforeEach
+    public void setUp(RestDocumentationContextProvider restDocumentation) {
+        RequestSpecification spec = new RequestSpecBuilder()
+                .addFilter(documentationConfiguration(restDocumentation))
+                .build();
+        DocumentUtils.setRequestSpecification(spec);
+    }
 
     @DisplayName("정상적인 회원가입 입력이 들어오면 회원 정보를 저장한다")
     @Test
@@ -45,7 +65,9 @@ class MemberControllerTest extends AcceptanceTest {
 
     private ExtractableResponse<Response> validateDuplicateEmail() {
         return RestAssured
-                .given().log().all()
+                .given(DocumentUtils.getRequestSpecification()).log().all()
+                .accept("application/json")
+                .filter(document("member", DocumentUtils.getRequestPreprocessor(), DocumentUtils.getResponsePreprocessor()))
                 .queryParam("email", EMAIL)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when().get("/api/members")
@@ -54,7 +76,9 @@ class MemberControllerTest extends AcceptanceTest {
 
     private ExtractableResponse<Response> saveMember(final MemberSaveRequest memberSaveRequest) {
         return RestAssured
-                .given().log().all()
+                .given(DocumentUtils.getRequestSpecification()).log().all()
+                .accept("application/json")
+                .filter(document("member", DocumentUtils.getRequestPreprocessor(), DocumentUtils.getResponsePreprocessor()))
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(memberSaveRequest)
                 .when().post("/api/members")
