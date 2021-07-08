ef7e875483d2a45dc5ffa73a313fd33004c1eaa7
xrabcde <xrabcde@gmail.com>
2021-07-08 14:59:15 +0900

    feat: 자동배포 shell script 추가 및 테스트 프로필 설정 (#31)
    Co-authored-by: xrabcde <xrabcde@gmail.com>
    
    Co-authored-by: sakjung <ssangyu123@gmail.com>

--- backend/src/test/java/com/woowacourse/zzimkkong/ZzimkkongApplicationTests.java
+++ backend/src/test/java/com/woowacourse/zzimkkong/ZzimkkongApplicationTests.java
@@ -2,8 +2,10 @@ package com.woowacourse.zzimkkong;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ZzimkkongApplicationTests {
 
     @Test
