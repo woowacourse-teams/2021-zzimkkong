9536f946dbd79ca67488963a7ec8b3034058511e
Sunny.K <sunnyk5780@gmail.com>
2021-07-09 11:04:54 +0900

    feat: 이메일 중복확인 기능 구현
    Co-authored-by: JO YUN HO <bedro27@gmail.com>
    Co-authored-by: Shim MunSeong <puterism.k@gmail.com>

--- frontend/src/pages/Join/Join.tsx
+++ frontend/src/pages/Join/Join.tsx
@@ -1,10 +1,46 @@
+import { useQuery } from 'react-query';
+import { AxiosError } from 'axios';
 import * as Styled from './Join.styles';
-import Header from 'components/Header/Header';
-import Layout from 'components/Layout/Layout';
-import Input from 'components/Input/Input';
+import { getValidateEmail } from 'api/join';
 import Button from 'components/Button/Button';
+import Header from 'components/Header/Header';
+import Input from 'components/Input/Input';
+import Layout from 'components/Layout/Layout';
+import MESSAGE from 'constants/message';
+import useInput from 'hooks/useInput';
 
 const Join = (): JSX.Element => {
+  const [email, onChangeEmail] = useInput('');
+
+  const isValidEmail = useQuery(['isValidEmail', email], getValidateEmail, {
+    enabled: false,
+    retry: false,
+  });
+
+  const getEmailMessage = () => {
+    const { isFetched, isError, isSuccess } = isValidEmail;
+    const error = isValidEmail.error as AxiosError;
+    const message = error?.response?.data?.message as string;
+
+    if (!isFetched) return '';
+
+    if (isSuccess) {
+      return MESSAGE.JOIN.VALID_EMAIL;
+    }
+
+    if (isError && message) {
+      return message;
+    }
+
+    return MESSAGE.JOIN.UNEXPECTED_ERROR;
+  };
+
+  const handleValidateEmail = () => {
+    if (!email) return;
+
+    isValidEmail.refetch();
+  };
+
   return (
     <>
       <Header />
@@ -12,10 +48,20 @@ const Join = (): JSX.Element => {
         <Styled.PageTitle>회원가입</Styled.PageTitle>
         <Styled.Container>
           <Styled.Form>
-            <Input type="email" label="이메일" autoFocus />
-            <Input type="password" label="비밀번호" />
-            <Input type="password" label="비밀번호 확인" />
-            <Input type="text" label="조직명" />
+            <Input
+              type="email"
+              label="이메일"
+              autoFocus
+              value={email}
+              onChange={onChangeEmail}
+              onBlur={handleValidateEmail}
+              message={getEmailMessage()}
+              status={isValidEmail.isError ? 'error' : 'success'}
+              required
+            />
+            <Input type="password" label="비밀번호" required />
+            <Input type="password" label="비밀번호 확인" required />
+            <Input type="text" label="조직명" required />
             <Button variant="primary" size="large" fullWidth>
               회원가입
             </Button>
