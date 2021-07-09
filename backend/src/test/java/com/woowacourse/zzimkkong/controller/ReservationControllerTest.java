a9eb665d324a9b9744cf05cdc9b159be717bba84
xrabcde <xrabcde@gmail.com>
2021-07-09 18:22:55 +0900

    fix: conflict 해결

---
+++ backend/src/test/java/com/woowacourse/zzimkkong/controller/ReservationControllerTest.java
@@ -0,0 +1,49 @@
+package com.woowacourse.zzimkkong.controller;
+
+import com.woowacourse.zzimkkong.dto.ReservationSaveRequest;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.time.LocalDateTime;
+
+import static com.woowacourse.zzimkkong.controller.DocumentUtils.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
+
+public class ReservationControllerTest extends AcceptanceTest {
+    @DisplayName("예약을 등록한다.")
+    @Test
+    void save() {
+        //given
+        ReservationSaveRequest reservationSaveRequest = new ReservationSaveRequest(
+                1L, //TODO: 나중에 인수테스트 전부 생기면 갖다 쓰기
+                LocalDateTime.of(2021, 5, 6, 16, 23, 0),
+                LocalDateTime.of(2021, 5, 6, 19, 23, 0),
+                "2345",
+                "sally",
+                "회의입니다."
+        );
+
+        //when
+        ExtractableResponse<Response> response = saveReservation(reservationSaveRequest);
+
+        //then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    private ExtractableResponse<Response> saveReservation(final ReservationSaveRequest reservationSaveRequest) {
+        return RestAssured
+                .given(getRequestSpecification()).log().all()
+                .accept("application/json")
+                .filter(document("reservation/post", getRequestPreprocessor(), getResponsePreprocessor()))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(reservationSaveRequest)
+                .when().post("/api/maps/1/reservations")
+                .then().log().all().extract();
+    }
+}
---
+++ backend/src/test/java/com/woowacourse/zzimkkong/repository/MapRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.woowacourse.zzimkkong.repository;
+
+import com.woowacourse.zzimkkong.domain.Map;
+import com.woowacourse.zzimkkong.domain.Member;
+import com.woowacourse.zzimkkong.exception.NoSuchMapException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MapRepositoryTest extends RepositoryTest {
+    public static final Member MEMBER = new Member("Sally@gmail.com", "1234", "Sally");
+    public static final Map MAP = new Map("롯데몰", MEMBER);
+
+    @Autowired
+    private MapRepository mapRepository;
+
+    @Test
+    @DisplayName("Map을 저장할 수 있다.")
+    void saveMap() {
+        //when
+        Map savedMap = mapRepository.save(MAP);
+
+        //then
+        assertThat(savedMap).isEqualTo(MAP);
+    }
+
+    @Test
+    @DisplayName("id로부터 저장된 Map을 찾아올 수 있다.")
+    void findById() {
+        //given
+        Map savedMap = mapRepository.save(MAP);
+
+        //when
+        Map findMap = mapRepository.findById(savedMap.getId())
+                .orElseThrow(NoSuchMapException::new);
+
+        //then
+        assertThat(savedMap).isEqualTo(findMap);
+    }
+}
