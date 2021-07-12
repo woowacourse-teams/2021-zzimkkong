5bca526410bf105ff5310eb6e233a147cafbe034
Kimun Kim <kkm97351@gmail.com>
2021-07-12 18:19:28 +0900

    feat: LoginInterceptor 생성 및 등록

--- backend/src/main/java/com/woowacourse/zzimkkong/WebConfig.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/WebConfig.java
@@ -1,15 +1,29 @@
 package com.woowacourse.zzimkkong;
 
+import com.woowacourse.zzimkkong.infrastructure.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    private final LoginInterceptor loginInterceptor;
+
+    public WebConfig(final LoginInterceptor loginInterceptor) {
+        this.loginInterceptor = loginInterceptor;
+    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("*");
 //                .allowedOriginPatterns("http://localhost:3000/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(this.loginInterceptor);
+        // todo 인가가 필요한 API url을 addPathPattern()로 등록하기
     }
 }
