9b18e0d6ba033b79902cb4062c445f800c0ca7a4
yujo <yujo.dev@gmail.com>
2021-07-12 18:24:03 +0900

    fix : 모바일 화면(320px)에서 validation 메세지가 깨지던 오류 해결

--- frontend/src/constants/message.ts
+++ frontend/src/constants/message.ts
@@ -4,9 +4,9 @@ const MESSAGE = {
     FAILURE: '회원가입에 실패했습니다.',
     VALID_EMAIL: '사용 가능한 이메일입니다.',
     VALID_PASSWORD: '사용 가능한 비밀번호입니다.',
-    INVALID_PASSWORD: '비밀번호는 영어와 숫자를 포함해서 8자 이상 20자 이내로 입력해주세요.',
-    VALID_PASSWORD_CONFIRM: '입력하신 비밀번호와 일치합니다.',
-    INVALID_PASSWORD_CONFIRM: '비밀번호와 비밀번호 확인란에 입력하신 문자가 다릅니다.',
+    INVALID_PASSWORD: '영어와 숫자를 포함하여 8~20자로 입력해주세요.',
+    VALID_PASSWORD_CONFIRM: '비밀번호가 일치합니다.',
+    INVALID_PASSWORD_CONFIRM: '비밀번호가 서로 다릅니다.',
     UNEXPECTED_ERROR: '이메일 중복 확인에 문제가 발생했습니다. 잠시 후에 다시 시도해주세요.',
   },
 };
