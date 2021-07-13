48092c7eca6d59ab399448129bb0004199df6058
Kimun Kim <kkm97351@gmail.com>
2021-07-13 14:22:29 +0900

    refactor: TokenExpiredException(유효기간 만료)의 예외 메세지 변경

--- backend/src/main/java/com/woowacourse/zzimkkong/exception/TokenExpiredException.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/exception/TokenExpiredException.java
@@ -3,7 +3,7 @@ package com.woowacourse.zzimkkong.exception;
 import org.springframework.http.HttpStatus;
 
 public class TokenExpiredException extends AuthorizationException {
-    private static final String MESSAGE = "토큰의 유효기간이 만료되었습니다.";
+    private static final String MESSAGE = "다시 로그인해주세요.";
 
     public TokenExpiredException() {
         super(MESSAGE, HttpStatus.UNAUTHORIZED);
