29131b5b438123e95da89fe9e254ccb9d1e1c9a0
y_woo :) <dusdn1702@naver.com>
2021-07-13 15:51:54 +0900

    test: 회원 로그인 service Test 구현

---
+++ backend/src/test/java/com/woowacourse/zzimkkong/service/AuthServiceTest.java
@@ -0,0 +1,73 @@
+package com.woowacourse.zzimkkong.service;
+
+import com.woowacourse.zzimkkong.domain.Member;
+import com.woowacourse.zzimkkong.dto.LoginRequest;
+import com.woowacourse.zzimkkong.dto.TokenResponse;
+import com.woowacourse.zzimkkong.exception.NoSuchMemberException;
+import com.woowacourse.zzimkkong.exception.PasswordMismatchException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+
+class AuthServiceTest extends ServiceTest {
+    public static final String EMAIL = "pobi@email.com";
+    public static final String PASSWORD = "test1234";
+    public static final String ORGANIZATION = "루터";
+    public static final Member MEMBER = new Member(EMAIL, PASSWORD, ORGANIZATION);
+
+    @Autowired
+    private AuthService authService;
+
+    @Test
+    @DisplayName("회원 로그인 요청이 옳다면 토큰을 발급한다.")
+    void login() {
+        //given
+        LoginRequest loginRequest = new LoginRequest(EMAIL, PASSWORD);
+        given(memberRepository.findByEmail(anyString()))
+                .willReturn(Optional.of(MEMBER));
+
+        //when
+        TokenResponse tokenResponse = authService.login(loginRequest);
+
+        //then
+        assertThat(tokenResponse).isNotNull();
+    }
+
+    @Test
+    @DisplayName("이메일이 존재하지 않는 회원이 회원 로그인을 요청한다면 오류가 발생한다.")
+    void loginException() {
+        //given
+        LoginRequest loginRequest = new LoginRequest(EMAIL, PASSWORD);
+
+        //when
+        given(memberRepository.findByEmail(anyString()))
+                .willReturn(Optional.empty());
+
+        //then
+        assertThatThrownBy(() -> authService.login(loginRequest))
+                .isInstanceOf(NoSuchMemberException.class);
+    }
+
+
+    @Test
+    @DisplayName("회원 로그인 요청 시, 이메일과 비밀번호가 일치하지 않으면 오류가 발생한다.")
+    void loginMismatchException() {
+        //given
+        LoginRequest loginRequest = new LoginRequest(EMAIL, PASSWORD);
+
+        //when
+        given(memberRepository.findByEmail(anyString()))
+                .willReturn(Optional.of(new Member(EMAIL, "real_password", ORGANIZATION)));
+
+        //then
+        assertThatThrownBy(() -> authService.login(loginRequest))
+                .isInstanceOf(PasswordMismatchException.class);
+    }
+}
