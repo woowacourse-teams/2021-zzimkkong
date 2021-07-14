5e4d64efe23fd8dee70ab82f5baaddd4499fc30b
y_woo :) <dusdn1702@naver.com>
2021-07-14 15:44:07 +0900

    test: 도메인 테스트 픽스처 분리와 리팩터링

--- backend/src/test/java/com/woowacourse/zzimkkong/domain/MemberTest.java
+++ backend/src/test/java/com/woowacourse/zzimkkong/domain/MemberTest.java
@@ -3,26 +3,23 @@ package com.woowacourse.zzimkkong.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static com.woowacourse.zzimkkong.CommonFixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MemberTest {
-    public static final String EMAIL = "pobi@email.com";
-    public static final String PASSWORD = "test1234";
-    public static final String ORGANIZATION = "루터";
-
     @Test
     @DisplayName("비밀번호가 일치하면 true를 반환한다.")
     void checkPassword() {
         Member member = new Member(EMAIL, PASSWORD, ORGANIZATION);
 
-        assertThat(member.checkPassword("test1234")).isTrue();
+        assertThat(member.checkPassword(PASSWORD)).isTrue();
     }
 
     @Test
-    @DisplayName("비밀번호가 일치하지 않으면 반환한다.")
+    @DisplayName("비밀번호가 일치하지 않으면 false를 반환한다.")
     void checkWrongPassword() {
         Member member = new Member(EMAIL, PASSWORD, ORGANIZATION);
 
-        assertThat(member.checkPassword("test123")).isFalse();
+        assertThat(member.checkPassword(PASSWORD+"%")).isFalse();
     }
 }
