7532ebf288383c511b76d62017d295ae574c6d9c
yujo <yujo.dev@gmail.com>
2021-07-14 23:13:41 +0900

    refactor: Modal이 상대 위치가 아닌 절대 위치에 배치 되도록 수정

--- frontend/src/components/Modal/Modal.styles.ts
+++ frontend/src/components/Modal/Modal.styles.ts
@@ -6,6 +6,9 @@ interface Props {
 
 export const Overlay = styled.div<Props>`
   display: ${({ open }) => (open ? 'flex' : 'none')};
+  position: fixed;
+  top: 0;
+  left: 0;
   width: 100vw;
   height: 100vh;
   justify-content: center;
