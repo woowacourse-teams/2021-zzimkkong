165d1721a066a63f48e2a567bd9f620795d5af0e
yujo <yujo.dev@gmail.com>
2021-07-17 22:55:24 +0900

    feat: Main 페이지 - 공간 목록 구현

---
+++ frontend/src/pages/Main/Main.styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const PageHeader = styled.h2`
+  font-size: 1.625rem;
+  font-weight: 700;
+  margin: 0.75rem 0 1.25rem;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  gap: 0.375rem;
+  flex-wrap: wrap;
+`;
+
+export const Wrapper = styled.div`
+  width: calc(50% - (0.375rem / 2));
+`;
