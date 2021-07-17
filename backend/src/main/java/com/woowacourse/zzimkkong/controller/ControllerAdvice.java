1dac0a0b999f971475a969814222cc7081ce854c
xrabcde <xrabcde@gmail.com>
2021-07-18 01:57:28 +0900

    refactor: ControllerAdvice 중복 핸들러코드 삭제

--- backend/src/main/java/com/woowacourse/zzimkkong/controller/ControllerAdvice.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/controller/ControllerAdvice.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.woowacourse.zzimkkong.dto.ErrorResponse;
 import com.woowacourse.zzimkkong.exception.ZzimkkongException;
 import com.woowacourse.zzimkkong.exception.authorization.AuthorizationException;
-import com.woowacourse.zzimkkong.exception.authorization.AuthorizationHeaderUninvolvedException;
 import com.woowacourse.zzimkkong.exception.map.MapException;
 import com.woowacourse.zzimkkong.exception.member.MemberException;
 import com.woowacourse.zzimkkong.exception.reservation.ReservationException;
@@ -12,7 +11,6 @@ import com.woowacourse.zzimkkong.exception.space.SpaceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -44,12 +42,6 @@ public class ControllerAdvice {
     public ResponseEntity<ErrorResponse> invalidParamHandler(final ConstraintViolationException exception) {
         logger.warn(exception.getMessage());
         return ResponseEntity.badRequest().body(ErrorResponse.of(exception));
-    }
-
-    @ExceptionHandler(AuthorizationHeaderUninvolvedException.class)
-    public ResponseEntity<ErrorResponse> invalidTokenHandler(final AuthorizationHeaderUninvolvedException exception) {
-        logger.warn(exception.getMessage());
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ErrorResponse.from(exception));
     }
 
     @ExceptionHandler(DataAccessException.class)
