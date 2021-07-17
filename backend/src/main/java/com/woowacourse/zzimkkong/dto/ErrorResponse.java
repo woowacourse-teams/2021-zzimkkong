8eb835a03f4af51e4457eb1e43ff8dfb9c860a2b
xrabcde <xrabcde@gmail.com>
2021-07-17 17:41:58 +0900

    feat: Valid exception 발생 시 발생한 필드도 같이 반환하도록 수정

--- backend/src/main/java/com/woowacourse/zzimkkong/dto/ErrorResponse.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/dto/ErrorResponse.java
@@ -1,5 +1,6 @@
 package com.woowacourse.zzimkkong.dto;
 
+import com.woowacourse.zzimkkong.exception.ZzimkkongException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import javax.validation.ConstraintViolationException;
@@ -8,13 +9,23 @@ import static com.woowacourse.zzimkkong.dto.Validator.FORMAT_MESSAGE;
 
 public class ErrorResponse {
     private String message;
+    private String field;
 
     public ErrorResponse() {
 
     }
 
     private ErrorResponse(final String message) {
+        this(message, null);
+    }
+
+    private ErrorResponse(final String message, final String field) {
         this.message = message;
+        this.field = field;
+    }
+
+    public static ErrorResponse of(final ZzimkkongException exception) {
+        return new ErrorResponse(exception.getMessage(), exception.getField());
     }
 
     public static ErrorResponse of(final RuntimeException exception) {
@@ -23,7 +34,8 @@ public class ErrorResponse {
 
     public static ErrorResponse of(final MethodArgumentNotValidException exception) {
         String message = exception.getBindingResult().getAllErrors().get(0).getDefaultMessage();
-        return new ErrorResponse(message);
+        String field = exception.getFieldErrors().get(0).getField();
+        return new ErrorResponse(message, field);
     }
 
     public static ErrorResponse of(final ConstraintViolationException exception) {
@@ -33,9 +45,5 @@ public class ErrorResponse {
 
     public static ErrorResponse invalidFormat() {
         return new ErrorResponse(FORMAT_MESSAGE);
-    }
-
-    public String getMessage() {
-        return message;
     }
 }
