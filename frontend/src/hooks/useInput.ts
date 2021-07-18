4616c5f712ea8f68acdbc17c9d366dff33b04a9f
Shim MunSeong <Puterism.K@gmail.com>
2021-07-18 22:42:01 +0900

    refactor: `useInput`이 제너릭이 아닌 string 타입의 값만 받도록 수정
    - `onChange` 메소드의 `event.target.value`가 항상 string 타입이기 때문에 실제 값은 string인 문제가 있어 `value`를 항상 string으로 받도록 수정함.

--- frontend/src/hooks/useInput.ts
+++ frontend/src/hooks/useInput.ts
@@ -1,12 +1,10 @@
 import { useState, ChangeEventHandler } from 'react';
 
-const useInput = <T extends unknown>(
-  initialValue: T
-): [T, ChangeEventHandler<HTMLInputElement>] => {
+const useInput = (initialValue = ''): [typeof value, ChangeEventHandler<HTMLInputElement>] => {
   const [value, setValue] = useState(initialValue);
 
   const onChange: ChangeEventHandler<HTMLInputElement> = (event) => {
-    setValue(event.target.value as T);
+    setValue(event.target.value);
   };
 
   return [value, onChange];
