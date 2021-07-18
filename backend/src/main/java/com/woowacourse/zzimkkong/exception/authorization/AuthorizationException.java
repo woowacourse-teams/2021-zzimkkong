6147133d0cb76de56b544edaf649ff0c4864aad5
xrabcde <xrabcde@gmail.com>
2021-07-18 22:17:42 +0900

    feat: 예외 발생 시 예외가 발생한 필드도 같이 반환 (#138)
    * feat: Valid exception 발생 시 발생한 필드도 같이 반환하도록 수정
    
    * feat: custom exception 발생 시 발생한 필드도 같이 반환하도록 수정
    
    * fix: errorResponse dto에 누락된 getter 추가
    
    * docs: 예외메시지 restDocs 수정
    
    * refactor: 코드 포맷팅
    
    * refactor: 중복되는 필드명 이넘으로 분리
    
    * refactor: ControllerAdvice 중복 핸들러코드 삭제
    
    * refactor: ExceptoinField 이넘 삭제, 상수로 리팩터링
    
    * refactor: NoDataToUpdateException은 필드반환하지 않도록 수정
    
    * chore: properties-prod의 ddl설정 none으로 수정

--- backend/src/main/java/com/woowacourse/zzimkkong/exception/authorization/AuthorizationException.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/exception/authorization/AuthorizationException.java
@@ -4,7 +4,9 @@ import com.woowacourse.zzimkkong.exception.ZzimkkongException;
 import org.springframework.http.HttpStatus;
 
 public class AuthorizationException extends ZzimkkongException {
+    private static final String TOKEN = "accessToken";
+
     public AuthorizationException(final String message, final HttpStatus httpStatus) {
-        super(message, httpStatus);
+        super(message, httpStatus, TOKEN);
     }
 }
