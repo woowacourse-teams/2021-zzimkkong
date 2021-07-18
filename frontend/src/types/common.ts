1cb6ce9ed86bb50e34e76bb176911b74174feeda
Shim MunSeong <Puterism.K@gmail.com>
2021-07-18 22:37:53 +0900

    feat: 공간 목록을 불러오는 `useSpaces` 커스텀 훅 구현

--- frontend/src/types/common.ts
+++ frontend/src/types/common.ts
@@ -1,3 +1,5 @@
+import { Color } from './styled';
+
 export interface Coordinate {
   x: number;
   y: number;
@@ -9,4 +11,12 @@ export interface Reservation {
   endDateTime: string;
   name: string;
   description: string;
+}
+
+export interface Space {
+  spaceId: number;
+  spaceName: string;
+  color: Color;
+  textPosition: 'left' | 'right' | 'top' | 'bottom';
+  coordinate: Coordinate;
 }
