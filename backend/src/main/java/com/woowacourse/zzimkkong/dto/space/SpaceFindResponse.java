2d326c04d93d2c476194532fe84109a11e31d7b2
Kimun Kim <kkm97351@gmail.com>
2021-07-18 19:21:18 +0900

    refactor: 공간 정보에 textPosition, color, coordinate를 같이 응답

--- backend/src/main/java/com/woowacourse/zzimkkong/dto/space/SpaceFindResponse.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/dto/space/SpaceFindResponse.java
@@ -5,17 +5,28 @@ import com.woowacourse.zzimkkong.domain.Space;
 public class SpaceFindResponse {
     private Long id;
     private String name;
+    private String textPosition;
+    private String color;
+    private String coordinate;
 
     private SpaceFindResponse() {
     }
 
-    private SpaceFindResponse(final Long id, final String name) {
+    private SpaceFindResponse(final Long id, final String name, String textPosition, String color, String coordinate) {
         this.id = id;
         this.name = name;
+        this.textPosition = textPosition;
+        this.color = color;
+        this.coordinate = coordinate;
     }
 
     public static SpaceFindResponse from(Space space) {
-        return new SpaceFindResponse(space.getId(), space.getName());
+        return new SpaceFindResponse(
+                space.getId(),
+                space.getName(),
+                space.getTextPosition(),
+                space.getColor(),
+                space.getCoordinate());
     }
 
     public Long getId() {
@@ -24,5 +35,17 @@ public class SpaceFindResponse {
 
     public String getName() {
         return name;
+    }
+
+    public String getTextPosition() {
+        return textPosition;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getCoordinate() {
+        return coordinate;
     }
 }
