fd78ff8cbdf124311d42b2566e85921ea496e489
xrabcde <xrabcde@gmail.com>
2021-07-20 11:19:02 +0900

    refactor: NoSuch 예외에 대해 예외메시지 통일

--- backend/src/main/java/com/woowacourse/zzimkkong/exception/map/NoSuchMapException.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/exception/map/NoSuchMapException.java
@@ -3,7 +3,7 @@ package com.woowacourse.zzimkkong.exception.map;
 import org.springframework.http.HttpStatus;
 
 public class NoSuchMapException extends MapException {
-    private static final String MESSAGE = "해당 맵이 존재하지 않습니다.";
+    private static final String MESSAGE = "존재하지 않는 맵입니다.";
 
     public NoSuchMapException() {
         super(MESSAGE, HttpStatus.BAD_REQUEST, MAP_ID);
--- backend/src/main/java/com/woowacourse/zzimkkong/exception/member/NoSuchMemberException.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/exception/member/NoSuchMemberException.java
@@ -3,7 +3,7 @@ package com.woowacourse.zzimkkong.exception.member;
 import org.springframework.http.HttpStatus;
 
 public class NoSuchMemberException extends MemberException {
-    private static final String MESSAGE = "이메일 혹은 비밀번호를 확인해주세요.";
+    private static final String MESSAGE = "존재하지 않는 회원입니다.";
 
     public NoSuchMemberException() {
         super(MESSAGE, HttpStatus.BAD_REQUEST, EMAIL);
