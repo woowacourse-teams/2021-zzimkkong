915b57d54dd4f74834396a93fd44a7e0c5357623
yujo <yujo.dev@gmail.com>
2021-07-24 21:37:53 +0900

    refactor: NotFound 페이지의 요소들이 정중앙에 오도록 변경

--- frontend/src/pages/NotFound/NotFound.styles.ts
+++ frontend/src/pages/NotFound/NotFound.styles.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
+  height: calc(100vh - 8rem);
   flex-direction: column;
   justify-content: center;
   align-items: center;
