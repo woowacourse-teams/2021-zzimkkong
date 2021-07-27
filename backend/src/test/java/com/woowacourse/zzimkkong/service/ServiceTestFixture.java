ef6f0331a4cdd9327dde0f3f22afdab11114ec5f
xrabcde <xrabcde@gmail.com>
2021-07-27 13:35:34 +0900

    refactor: timezone offset 상수화 (#224)
    * fix: 예약시간에 대한 검증을 KST 기준으로 하도록 변경
    
    * fix: timezone ID 수정
    
    * fix: local time만 KST로 timezone 설정
    
    * fix: remote brnach와 병합 해결
    
    * refactor: timezone offset 상수화
    
    Co-authored-by: sakjung <ssangyu123@gmail.com>

--- backend/src/test/java/com/woowacourse/zzimkkong/service/ServiceTestFixture.java
+++ backend/src/test/java/com/woowacourse/zzimkkong/service/ServiceTestFixture.java
@@ -12,6 +12,7 @@ public class ServiceTestFixture {
     public static final String EMAIL = "pobi@email.com";
     public static final String PASSWORD = "test1234";
     public static final String ORGANIZATION = "우아한테크코스";
+    public static final Integer TIMEZONE_OFFSET = 9;
     public static Member POBI = new Member(1L, EMAIL, PASSWORD, ORGANIZATION);
 
     public static Map LUTHER = new Map(1L, "루터회관", POBI);
