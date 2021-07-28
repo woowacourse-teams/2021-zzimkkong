b3035441809d69e250a4928c12160d2cde2f52a2
Shim MunSeong <Puterism.K@gmail.com>
2021-07-28 12:07:18 +0900

    feat: `Styled.Editor`에 지도가 그려질 SVG 영역 배치 및 위치 설정
    Co-authored-by: JO YUN HO <bedro27@gmail.com>
    Co-authored-by: Sunny K <sunnyk5780@gmail.com>

--- frontend/src/pages/ManagerMapCreate/ManagerMapCreate.tsx
+++ frontend/src/pages/ManagerMapCreate/ManagerMapCreate.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { ReactComponent as EditIcon } from 'assets/svg/edit.svg';
 import Button from 'components/Button/Button';
 import Header from 'components/Header/Header';
 import Layout from 'components/Layout/Layout';
+import PALETTE from 'constants/palette';
+import useInput from 'hooks/useInput';
 import * as Styled from './ManagerMapCreate.styles';
 
+const GRID_SIZE = 10;
+
 const ManagerMapCreate = (): JSX.Element => {
+  const editorRef = useRef<HTMLDivElement | null>(null);
+
+  const [widthValue, onChangeWidthValue] = useInput('800');
+  const [heightValue, onChangeHeightValue] = useInput('600');
+
+  const width = Number(widthValue);
+  const height = Number(heightValue);
+
+  const [board, setBoard] = useState({
+    x: 0,
+    y: 0,
+    scale: 1,
+  });
+
+  useEffect(() => {
+    const editorWidth = editorRef.current ? editorRef.current.offsetWidth : 0;
+    const editorHeight = editorRef.current ? editorRef.current.offsetHeight : 0;
+
+    setBoard((prevBoard) => ({
+      ...prevBoard,
+      x: (editorWidth - width) / 2,
+      y: (editorHeight - height) / 2,
+    }));
+  }, [width, height]);
+
   return (
     <>
       <Header />
@@ -38,22 +67,72 @@ const ManagerMapCreate = (): JSX.Element => {
               <EditIcon />
               <EditIcon />
             </Styled.Toolbar>
-            <Styled.Editor></Styled.Editor>
+            <Styled.Editor ref={editorRef}>
+              <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100%">
+                <rect width="100%" height="100%" fill={PALETTE.GRAY[200]}></rect>
+                <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+                  <defs>
+                    <pattern
+                      id="smallGrid"
+                      width={`${GRID_SIZE}px`}
+                      height={`${GRID_SIZE}px`}
+                      patternUnits="userSpaceOnUse"
+                    >
+                      <path
+                        d={`M ${GRID_SIZE} 0 L 0 0 0 ${GRID_SIZE}`}
+                        fill="none"
+                        stroke="gray"
+                        strokeWidth="0.5"
+                      />
+                    </pattern>
+                    <pattern
+                      id="grid"
+                      width={`${GRID_SIZE * 10}px`}
+                      height={`${GRID_SIZE * 10}px`}
+                      patternUnits="userSpaceOnUse"
+                    >
+                      <rect
+                        width={`${GRID_SIZE * 10}px`}
+                        height={`${GRID_SIZE * 10}px`}
+                        fill="url(#smallGrid)"
+                      />
+                      <path
+                        d={`M ${GRID_SIZE * 10} 0 L 0 0 0 ${GRID_SIZE * 10}`}
+                        fill="none"
+                        stroke="gray"
+                        strokeWidth="1"
+                      />
+                    </pattern>
+                  </defs>
+                  <g
+                    id="board"
+                    transform={`matrix(${board.scale}, 0, 0, ${board.scale}, ${board.x}, ${board.y})`}
+                  >
+                    <rect width={`${width}px`} height={`${height}px`} fill="white" />
+                    {/* 전체 격자를 그리는 rect */}
+                    <rect
+                      width={`${width + 0.5}px`}
+                      height={`${height + 0.5}px`}
+                      fill="url(#grid)"
+                    />
+                  </g>
+                </svg>
+              </svg>
+            </Styled.Editor>
             <Styled.Toolbar>
               <Styled.InputWrapper>
                 <Styled.Label>
                   <Styled.LabelIcon>W</Styled.LabelIcon>
                   <Styled.LabelText>넓이</Styled.LabelText>
                 </Styled.Label>
-                <Styled.SizeInput value="8000" />
+                <Styled.SizeInput value={widthValue} onChange={onChangeWidthValue} />
               </Styled.InputWrapper>
               <Styled.InputWrapper>
                 <Styled.Label>
                   <Styled.LabelIcon>H</Styled.LabelIcon>
                   <Styled.LabelText>높이</Styled.LabelText>
                 </Styled.Label>
-
-                <Styled.SizeInput value="6000" />
+                <Styled.SizeInput value={heightValue} onChange={onChangeHeightValue} />
               </Styled.InputWrapper>
             </Styled.Toolbar>
           </Styled.EditorContent>
