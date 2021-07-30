1f0a4da5b18f14a469ab1e1cb557d5f2766ba71c
yujo <yujo.dev@gmail.com>
2021-07-30 16:30:35 +0900

    feat: 토큰을 검증하는 api 코드 작성

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -9,3 +9,7 @@ interface LoginParams {
 export const postLogin = (loginData: LoginParams): Promise<AxiosResponse> => {
   return api.post('/login/token', loginData);
 };
+
+export const postTokenValidation = (): Promise<AxiosResponse> => {
+  return api.post(`/members/token`);
+};
