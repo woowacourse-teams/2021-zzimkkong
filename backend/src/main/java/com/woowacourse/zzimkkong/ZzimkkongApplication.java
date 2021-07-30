8b3900aa4411e15bff016c7b13d5b15195d0ed46
sakjung <ssangyu123@gmail.com>
2021-07-30 21:07:05 +0900

    chore: jenkins build 촉발

--- backend/src/main/java/com/woowacourse/zzimkkong/ZzimkkongApplication.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/ZzimkkongApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ZzimkkongApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(ZzimkkongApplication.class, args);
     }
 }
