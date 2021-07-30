a90dcff068e3a705f0a014da067302cb9bad8e13
Kimun Kim <kkm97351@gmail.com>
2021-07-30 13:07:10 +0900

    refactor: S3UploaderTest, SvgConverterTest 불필요 변수 삭제

--- backend/src/test/java/com/woowacourse/zzimkkong/infrastructure/S3UploaderTest.java
+++ backend/src/test/java/com/woowacourse/zzimkkong/infrastructure/S3UploaderTest.java
@@ -25,18 +25,17 @@ class S3UploaderTest {
     @Autowired
     private SvgConverter svgConverter;
 
-    private File fileToDelete;
+    private File testFile;
 
     @Test
     @DisplayName("파일을 업로드한 후 url을 받아온다.")
     void upload() {
         // given
         String rawSvgData = "<?xml version=\"1.0\"?><svg fill=\"#000000\" xmlns=\"http://www.w3.org/2000/svg\"  viewBox=\"0 0 30 30\" width=\"30px\" height=\"30px\">    <path d=\"M 7 4 C 6.744125 4 6.4879687 4.0974687 6.2929688 4.2929688 L 4.2929688 6.2929688 C 3.9019687 6.6839688 3.9019687 7.3170313 4.2929688 7.7070312 L 11.585938 15 L 4.2929688 22.292969 C 3.9019687 22.683969 3.9019687 23.317031 4.2929688 23.707031 L 6.2929688 25.707031 C 6.6839688 26.098031 7.3170313 26.098031 7.7070312 25.707031 L 15 18.414062 L 22.292969 25.707031 C 22.682969 26.098031 23.317031 26.098031 23.707031 25.707031 L 25.707031 23.707031 C 26.098031 23.316031 26.098031 22.682969 25.707031 22.292969 L 18.414062 15 L 25.707031 7.7070312 C 26.098031 7.3170312 26.098031 6.6829688 25.707031 6.2929688 L 23.707031 4.2929688 C 23.316031 3.9019687 22.682969 3.9019687 22.292969 4.2929688 L 15 11.585938 L 7.7070312 4.2929688 C 7.5115312 4.0974687 7.255875 4 7 4 z\"/></svg>";
-        File testImagePngFile = svgConverter.convertSvgToPngFile(rawSvgData, "testImageFileName");
-        fileToDelete = testImagePngFile;
+        this.testFile = svgConverter.convertSvgToPngFile(rawSvgData, "testImageFileName");
 
         // when
-        String url = s3Uploader.upload("testDirectoryName", testImagePngFile);
+        String url = s3Uploader.upload("testDirectoryName", testFile);
         Matcher matcher = URL_PATTERN.matcher(url);
 
         // then
@@ -45,6 +44,6 @@ class S3UploaderTest {
 
     @AfterEach
     void deleteFile() {
-        fileToDelete.delete();
+        testFile.delete();
     }
 }
