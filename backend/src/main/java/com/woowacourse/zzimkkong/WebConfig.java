4d6a5aa51eb2f0566e6d5af727ce977a088deb44
sakjung <ssangyu123@gmail.com>
2021-07-31 20:23:47 +0900

    fix: locaiton header 관련 cors 이슈 해결

--- backend/src/main/java/com/woowacourse/zzimkkong/WebConfig.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/WebConfig.java
@@ -1,5 +1,7 @@
 package com.woowacourse.zzimkkong;
 
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.methods.HttpHead;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,7 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedMethods("*");
+                .allowedMethods("*")
+                .exposedHeaders(HttpHeaders.LOCATION);
 //                .allowedOriginPatterns("https://zzimkkong-proxy.o-r.kr/");
     }
 }
