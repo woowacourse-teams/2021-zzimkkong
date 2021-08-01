483013f15dc50d963293c2fdc79985a8b43b99c4
Yeonwoo Cho <dusdn1702@gmail.com>
2021-08-01 23:55:23 +0900

    refactor: 현재 시작시간 예약 생성을 허용한다. (#281)

--- backend/src/test/java/com/woowacourse/zzimkkong/service/ServiceTest.java
+++ backend/src/test/java/com/woowacourse/zzimkkong/service/ServiceTest.java
@@ -1,10 +1,12 @@
 package com.woowacourse.zzimkkong.service;
 
 import com.woowacourse.zzimkkong.infrastructure.StorageUploader;
+import com.woowacourse.zzimkkong.infrastructure.TimeConverter;
 import com.woowacourse.zzimkkong.repository.MapRepository;
 import com.woowacourse.zzimkkong.repository.MemberRepository;
 import com.woowacourse.zzimkkong.repository.ReservationRepository;
 import com.woowacourse.zzimkkong.repository.SpaceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
@@ -28,4 +30,7 @@ public class ServiceTest {
 
     @MockBean
     protected StorageUploader storageUploader;
+
+    @Autowired
+    protected TimeConverter timeConverter;
 }
