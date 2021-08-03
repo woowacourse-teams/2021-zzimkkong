e4541702f600dbd855f774e038f673489df03aa0
xrabcde <xrabcde@gmail.com>
2021-08-03 14:47:06 +0900

    docs: db 더미데이터 insert문 추가

--- backend/src/main/resources/db/migration/V1__init.sql
+++ backend/src/main/resources/db/migration/V1__init.sql
@@ -49,3 +49,51 @@ create table reservation (
     space_id bigint not null,
     foreign key (space_id) references space (id)
 );
+
+insert into member (id, email, password, organization)
+    value (1, "pobi@woowa.com", "test1234", "woowacourse");
+
+insert into map (id, name, map_drawing, map_image_url, member_id)
+    value (1, "루터회관", "abcde", "https://d1dgzmdd5f1fx6.cloudfront.net/thumbnails/1.png", 1);
+
+insert into space (id, area, color, coordinate, description, map_image, name, available_end_time, available_start_time, disabled_weekdays, reservation_enable, reservation_maximum_time_unit, reservation_minimum_time_unit, reservation_time_unit, text_position, map_id)
+    value (1, "area", "#FFE3AC", "29, 229", "description", "map_image", "회의실1", "23:59:59", "00:00", null, true, 1440, 10, 10, "bottom", 1);
+
+insert into space (id, area, color, coordinate, description, map_image, name, available_end_time, available_start_time, disabled_weekdays, reservation_enable, reservation_maximum_time_unit, reservation_minimum_time_unit, reservation_time_unit, text_position, map_id)
+    value (2, "area", "#FFE3AC", "88, 229", "description", "map_image", "회의실2", "23:59:59", "00:00", null, true, 1440, 10, 10, "bottom", 1);
+
+insert into space (id, area, color, coordinate, description, map_image, name, available_end_time, available_start_time, disabled_weekdays, reservation_enable, reservation_maximum_time_unit, reservation_minimum_time_unit, reservation_time_unit, text_position, map_id)
+    value (3, "area", "#FFE3AC", "510, 220", "description", "map_image", "회의실3", "23:59:59", "00:00", null, true, 1440, 10, 10, "bottom", 1);
+
+insert into space (id, area, color, coordinate, description, map_image, name, available_end_time, available_start_time, disabled_weekdays, reservation_enable, reservation_maximum_time_unit, reservation_minimum_time_unit, reservation_time_unit, text_position, map_id)
+    value (4, "area", "#FFE3AC", "584, 220", "description", "map_image", "회의실4", "23:59:59", "00:00", null, true, 1440, 10, 10, "bottom", 1);
+
+insert into space (id, area, color, coordinate, description, map_image, name, available_end_time, available_start_time, disabled_weekdays, reservation_enable, reservation_maximum_time_unit, reservation_minimum_time_unit, reservation_time_unit, text_position, map_id)
+    value (5, "area", "#FFE3AC", "668, 335", "description", "map_image", "회의실5", "23:59:59", "00:00", null, true, 1440, 10, 10, "bottom", 1);
+
+insert into space (id, area, color, coordinate, description, map_image, name, available_end_time, available_start_time, disabled_weekdays, reservation_enable, reservation_maximum_time_unit, reservation_minimum_time_unit, reservation_time_unit, text_position, map_id)
+    value (6, "area", "#CCDFFB", "208, 289", "description", "map_image", "페어룸1", "23:59:59", "00:00", null, true, 1440, 10, 10, "left", 1);
+
+insert into space (id, area, color, coordinate, description, map_image, name, available_end_time, available_start_time, disabled_weekdays, reservation_enable, reservation_maximum_time_unit, reservation_minimum_time_unit, reservation_time_unit, text_position, map_id)
+    value (7, "area", "#CCDFFB", "208, 318", "description", "map_image", "페어룸2", "23:59:59", "00:00", null, true, 1440, 10, 10, "left", 1);
+
+insert into space (id, area, color, coordinate, description, map_image, name, available_end_time, available_start_time, disabled_weekdays, reservation_enable, reservation_maximum_time_unit, reservation_minimum_time_unit, reservation_time_unit, text_position, map_id)
+    value (8, "area", "#CCDFFB", "208, 347", "description", "map_image", "페어룸3", "23:59:59", "00:00", null, true, 1440, 10, 10, "left", 1);
+
+insert into space (id, area, color, coordinate, description, map_image, name, available_end_time, available_start_time, disabled_weekdays, reservation_enable, reservation_maximum_time_unit, reservation_minimum_time_unit, reservation_time_unit, text_position, map_id)
+    value (9, "area", "#CCDFFB", "208, 376", "description", "map_image", "페어룸4", "23:59:59", "00:00", null, true, 1440, 10, 10, "left", 1);
+
+insert into space (id, area, color, coordinate, description, map_image, name, available_end_time, available_start_time, disabled_weekdays, reservation_enable, reservation_maximum_time_unit, reservation_minimum_time_unit, reservation_time_unit, text_position, map_id)
+    value (10, "area", "#CCDFFB", "208, 404", "description", "map_image", "페어룸5", "23:59:59", "00:00", null, true, 1440, 10, 10, "left", 1);
+
+insert into space (id, area, color, coordinate, description, map_image, name, available_end_time, available_start_time, disabled_weekdays, reservation_enable, reservation_maximum_time_unit, reservation_minimum_time_unit, reservation_time_unit, text_position, map_id)
+    value (11, "area", "#D8FBCC", "259, 336", "description", "map_image", "트랙방", "23:59:59", "00:00", null, true, 1440, 10, 10, "bottom", 1);
+
+insert into space (id, area, color, coordinate, description, map_image, name, available_end_time, available_start_time, disabled_weekdays, reservation_enable, reservation_maximum_time_unit, reservation_minimum_time_unit, reservation_time_unit, text_position, map_id)
+    value (12, "area", "#FED7D9", "100, 90", "description", "map_image", "백엔드 강의장", "23:59:59", "00:00", null, true, 1440, 10, 10, "bottom", 1);
+
+insert into space (id, area, color, coordinate, description, map_image, name, available_end_time, available_start_time, disabled_weekdays, reservation_enable, reservation_maximum_time_unit, reservation_minimum_time_unit, reservation_time_unit, text_position, map_id)
+    value (13, "area", "#FED7D9", "560, 40", "description", "map_image", "프론트엔드 강의장1", "23:59:59", "00:00", null, true, 1440, 10, 10, "bottom", 1);
+
+insert into space (id, area, color, coordinate, description, map_image, name, available_end_time, available_start_time, disabled_weekdays, reservation_enable, reservation_maximum_time_unit, reservation_minimum_time_unit, reservation_time_unit, text_position, map_id)
+    value (14, "area", "#FED7D9", "560, 140", "description", "map_image", "프론트엔드 강의장2", "23:59:59", "00:00", null, true, 1440, 10, 10, "bottom", 1);
