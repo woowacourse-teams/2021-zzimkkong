021ac21949044693cb3a544b9d32cc435f5abf45
sakjung <ssangyu123@gmail.com>
2021-08-04 21:51:35 +0900

    refactor: repository test 정리

--- backend/src/test/java/com/woowacourse/zzimkkong/repository/ReservationRepositoryTest.java
+++ backend/src/test/java/com/woowacourse/zzimkkong/repository/ReservationRepositoryTest.java
@@ -1,6 +1,6 @@
 package com.woowacourse.zzimkkong.repository;
 
-import com.woowacourse.zzimkkong.domain.Reservation;
+import com.woowacourse.zzimkkong.domain.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,22 +10,107 @@ import org.junit.jupiter.params.provider.CsvSource;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static com.woowacourse.zzimkkong.CommonFixture.*;
+import static com.woowacourse.zzimkkong.Constants.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class ReservationRepositoryTest extends RepositoryTest {
+    private Space be;
+    private Space fe;
+
+    private Reservation beAmZeroOne;
+    private Reservation bePmOneTwo;
+    private Reservation beNextDayAmSixTwelve;
+    private Reservation fe1ZeroOne;
+
     @BeforeEach
     void setUp() {
-        members.save(POBI);
-        maps.save(LUTHER);
-        spaces.save(BE);
-        spaces.save(FE1);
+        Member pobi = new Member(EMAIL, PASSWORD, ORGANIZATION);
+        Map luther = new Map(LUTHER_NAME, MAP_DRAWING_DATA, MAP_IMAGE_URL, pobi);
 
-        reservations.save(BE_AM_ZERO_ONE);
-        reservations.save(BE_PM_ONE_TWO);
-        reservations.save(BE_NEXT_DAY_PM_SIX_TWELVE);
-        reservations.save(FE1_ZERO_ONE);
+        Setting beSetting = new Setting.Builder()
+                .availableStartTime(BE_AVAILABLE_START_TIME)
+                .availableEndTime(BE_AVAILABLE_END_TIME)
+                .reservationTimeUnit(BE_RESERVATION_TIME_UNIT)
+                .reservationMinimumTimeUnit(BE_RESERVATION_MINIMUM_TIME_UNIT)
+                .reservationMaximumTimeUnit(BE_RESERVATION_MAXIMUM_TIME_UNIT)
+                .reservationEnable(BE_RESERVATION_ENABLE)
+                .disabledWeekdays(BE_ENABLED_DAY_OF_WEEK)
+                .build();
+
+        be = new Space.Builder()
+                .name(BE_NAME)
+                .color(BE_COLOR)
+                .map(luther)
+                .description(BE_DESCRIPTION)
+                .area(SPACE_DRAWING)
+                .setting(beSetting)
+                .build();
+
+        Setting feSetting = new Setting.Builder()
+                .availableStartTime(FE_AVAILABLE_START_TIME)
+                .availableEndTime(FE_AVAILABLE_END_TIME)
+                .reservationTimeUnit(FE_RESERVATION_TIME_UNIT)
+                .reservationMinimumTimeUnit(FE_RESERVATION_MINIMUM_TIME_UNIT)
+                .reservationMaximumTimeUnit(FE_RESERVATION_MAXIMUM_TIME_UNIT)
+                .reservationEnable(FE_RESERVATION_ENABLE)
+                .disabledWeekdays(FE_ENABLED_DAY_OF_WEEK)
+                .build();
+
+        fe = new Space.Builder()
+                .name(FE_NAME)
+                .color(FE_COLOR)
+                .map(luther)
+                .description(FE_DESCRIPTION)
+                .area(SPACE_DRAWING)
+                .setting(feSetting)
+                .build();
+
+        members.save(pobi);
+        maps.save(luther);
+        spaces.save(be);
+        spaces.save(fe);
+
+        beAmZeroOne = new Reservation.Builder()
+                .startTime(BE_AM_ZERO_ONE_START_TIME)
+                .endTime(BE_AM_ZERO_ONE_END_TIME)
+                .description(BE_AM_ZERO_ONE_DESCRIPTION)
+                .userName(BE_AM_ZERO_ONE_USERNAME)
+                .password(BE_AM_ZERO_ONE_PASSWORD)
+                .space(be)
+                .build();
+
+        bePmOneTwo = new Reservation.Builder()
+                .startTime(BE_PM_ONE_TWO_START_TIME)
+                .endTime(BE_PM_ONE_TWO_END_TIME)
+                .description(BE_PM_ONE_TWO_DESCRIPTION)
+                .userName(BE_PM_ONE_TWO_USERNAME)
+                .password(BE_PM_ONE_TWO_PASSWORD)
+                .space(be)
+                .build();
+
+        beNextDayAmSixTwelve = new Reservation.Builder()
+                .startTime(BE_NEXT_DAY_AM_SIX_TWELVE_START_TIME)
+                .endTime(BE_NEXT_DAY_AM_SIX_TWELVE_END_TIME)
+                .description(BE_NEXT_DAY_AM_SIX_TWELVE_DESCRIPTION)
+                .userName(BE_NEXT_DAY_AM_SIX_TWELVE_USERNAME)
+                .password(BE_NEXT_DAY_AM_SIX_TWELVE_PASSWORD)
+                .space(be)
+                .build();
+
+        fe1ZeroOne = new Reservation.Builder()
+                .startTime(FE1_ZERO_ONE_START_TIME)
+                .endTime(FE1_ZERO_ONE_END_TIME)
+                .description(FE1_ZERO_ONE_DESCRIPTION)
+                .userName(FE1_ZERO_ONE_USERNAME)
+                .password(FE1_ZERO_ONE_PASSWORD)
+                .space(fe)
+                .build();
+
+        reservations.save(beAmZeroOne);
+        reservations.save(bePmOneTwo);
+        reservations.save(beNextDayAmSixTwelve);
+        reservations.save(fe1ZeroOne);
     }
 
     @Test
@@ -33,12 +118,12 @@ class ReservationRepositoryTest extends RepositoryTest {
     void save() {
         //given
         Reservation be_two_three = new Reservation.Builder()
-                .startTime(THE_DAY_AFTER_TOMORROW_START_TIME.plusHours(2))
-                .endTime(THE_DAY_AFTER_TOMORROW_START_TIME.plusHours(3))
+                .startTime(THE_DAY_AFTER_TOMORROW.atTime(2, 0))
+                .endTime(THE_DAY_AFTER_TOMORROW.atTime(3, 0))
                 .description("찜꽁 4차 회의")
                 .userName("찜꽁")
                 .password("1234")
-                .space(BE)
+                .space(be)
                 .build();
 
         //when
@@ -54,13 +139,13 @@ class ReservationRepositoryTest extends RepositoryTest {
     void findAllBySpaceIdAndStartTimeIsBetweenAndEndTimeIsBetween() {
         // given, when
         List<Reservation> foundReservations = getReservations(
-                List.of(BE.getId(), FE1.getId()),
-                THE_DAY_AFTER_TOMORROW_START_TIME,
-                THE_DAY_AFTER_TOMORROW_START_TIME.plusHours(14));
+                List.of(be.getId(), fe.getId()),
+                THE_DAY_AFTER_TOMORROW.atTime(0, 0),
+                THE_DAY_AFTER_TOMORROW.atTime(14, 0));
 
         // then
         assertThat(foundReservations).usingRecursiveComparison()
-                .isEqualTo(List.of(BE_AM_ZERO_ONE, BE_PM_ONE_TWO, FE1_ZERO_ONE));
+                .isEqualTo(List.of(beAmZeroOne, bePmOneTwo, fe1ZeroOne));
     }
 
     @DisplayName("특정 시간에 부합하는 예약이 없으면 빈 리스트를 반환한다")
@@ -68,9 +153,9 @@ class ReservationRepositoryTest extends RepositoryTest {
     void findAllBySpaceIdAndStartTimeIsBetweenAndEndTimeIsBetween_noMatchingTime() {
         // given, when
         List<Reservation> foundReservations = getReservations(
-                List.of(BE.getId()),
-                THE_DAY_AFTER_TOMORROW_START_TIME.plusHours(15),
-                THE_DAY_AFTER_TOMORROW_START_TIME.plusHours(18));
+                List.of(be.getId()),
+                THE_DAY_AFTER_TOMORROW.atTime(15, 0),
+                THE_DAY_AFTER_TOMORROW.atTime(18, 0));
 
         // then
         assertThat(foundReservations).isEmpty();
@@ -81,9 +166,9 @@ class ReservationRepositoryTest extends RepositoryTest {
     void findAllBySpaceIdAndStartTimeIsBetweenAndEndTimeIsBetween_noMatchingReservation() {
         // given, when
         List<Reservation> foundReservations = getReservations(
-                List.of(FE1.getId()),
-                THE_DAY_AFTER_TOMORROW_START_TIME.plusHours(13),
-                THE_DAY_AFTER_TOMORROW_START_TIME.plusHours(14));
+                List.of(fe.getId()),
+                THE_DAY_AFTER_TOMORROW.atTime(13, 0),
+                THE_DAY_AFTER_TOMORROW.atTime(14, 0));
 
         // then
         assertThat(foundReservations).isEmpty();
@@ -94,30 +179,40 @@ class ReservationRepositoryTest extends RepositoryTest {
     void findAllBySpaceIdAndStartTimeIsBetweenAndEndTimeIsBetween_allSpaces() {
         // given, when
         List<Reservation> foundReservations = getReservations(
-                List.of(BE.getId(), FE1.getId()),
-                THE_DAY_AFTER_TOMORROW_START_TIME,
-                THE_DAY_AFTER_TOMORROW_START_TIME.plusDays(1));
+                List.of(be.getId(), fe.getId()),
+                THE_DAY_AFTER_TOMORROW.atTime(0,0),
+                THE_DAY_AFTER_TOMORROW.atTime(0,0).plusDays(1));
 
         // then
-        assertThat(foundReservations).usingRecursiveComparison()
-                .isEqualTo(List.of(BE_AM_ZERO_ONE, BE_PM_ONE_TWO, FE1_ZERO_ONE));
+        assertThat(foundReservations).containsExactlyInAnyOrderElementsOf(List.of(beAmZeroOne, bePmOneTwo, fe1ZeroOne));
     }
 
     @DisplayName("예약을 삭제할 수 있다.")
     @Test
     void delete() {
         //given, when, then
-        assertDoesNotThrow(() -> reservations.delete(BE_NEXT_DAY_PM_SIX_TWELVE));
+        assertDoesNotThrow(() -> reservations.delete(beNextDayAmSixTwelve));
     }
 
     @DisplayName("해당 공간에 대한 예약 존재여부를 확인한다.")
     @Test
     void existsBySpace() {
         // given, when, then
-        assertThat(reservations.existsBySpaceIdAndEndTimeAfter(FE1.getId(), LocalDateTime.now())).isTrue();
+        assertThat(reservations.existsBySpaceIdAndEndTimeAfter(fe.getId(), LocalDateTime.now())).isTrue();
 
-        reservations.delete(FE1_ZERO_ONE);
-        assertThat(reservations.existsBySpaceIdAndEndTimeAfter(FE1.getId(), LocalDateTime.now())).isFalse();
+        reservations.delete(fe1ZeroOne);
+        assertThat(reservations.existsBySpaceIdAndEndTimeAfter(fe.getId(), LocalDateTime.now())).isFalse();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0:false", "30:true"}, delimiter = ':')
+    @DisplayName("특정 시간 이후의 예약이 존재하는지 확인한다.")
+    void existsAllStartTimeAfter(int minusMinute, boolean expected) {
+        //given, when
+        Boolean actual = reservations.existsBySpaceIdAndEndTimeAfter(be.getId(), BE_NEXT_DAY_AM_SIX_TWELVE_END_TIME.minusMinutes(minusMinute));
+
+        //then
+        assertThat(actual).isEqualTo(expected);
     }
 
     private List<Reservation> getReservations(List<Long> spaceIds, LocalDateTime minimumDateTime, LocalDateTime maximumDateTime) {
@@ -128,16 +223,5 @@ class ReservationRepositoryTest extends RepositoryTest {
                 minimumDateTime,
                 maximumDateTime
         );
-    }
-
-    @ParameterizedTest
-    @CsvSource(value = {"0:false", "30:true"}, delimiter = ':')
-    @DisplayName("특정 시간 이후의 예약이 존재하는지 확인한다.")
-    void existsAllStartTimeAfter(int minusMinute, boolean expected) {
-        //given, when
-        Boolean actual = reservations.existsBySpaceIdAndEndTimeAfter(BE.getId(), BE_NEXT_DAY_PM_SIX_TWELVE.getEndTime().minusMinutes(minusMinute));
-
-        //then
-        assertThat(actual).isEqualTo(expected);
     }
 }
--- backend/src/test/java/com/woowacourse/zzimkkong/repository/SpaceRepositoryTest.java
+++ backend/src/test/java/com/woowacourse/zzimkkong/repository/SpaceRepositoryTest.java
@@ -1,5 +1,8 @@
 package com.woowacourse.zzimkkong.repository;
 
+import com.woowacourse.zzimkkong.domain.Map;
+import com.woowacourse.zzimkkong.domain.Member;
+import com.woowacourse.zzimkkong.domain.Setting;
 import com.woowacourse.zzimkkong.domain.Space;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -7,40 +10,83 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static com.woowacourse.zzimkkong.CommonFixture.*;
+import static com.woowacourse.zzimkkong.Constants.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class SpaceRepositoryTest extends RepositoryTest {
+    private Map luther;
+    private Space be;
+    private Space fe;
+
     @BeforeEach
     void setUp() {
-        members.save(POBI);
-        maps.save(LUTHER);
+        Member pobi = new Member(EMAIL, PASSWORD, ORGANIZATION);
+        luther = new Map(LUTHER_NAME, MAP_DRAWING_DATA, MAP_IMAGE_URL, pobi);
+
+        Setting beSetting = new Setting.Builder()
+                .availableStartTime(BE_AVAILABLE_START_TIME)
+                .availableEndTime(BE_AVAILABLE_END_TIME)
+                .reservationTimeUnit(BE_RESERVATION_TIME_UNIT)
+                .reservationMinimumTimeUnit(BE_RESERVATION_MINIMUM_TIME_UNIT)
+                .reservationMaximumTimeUnit(BE_RESERVATION_MAXIMUM_TIME_UNIT)
+                .reservationEnable(BE_RESERVATION_ENABLE)
+                .disabledWeekdays(BE_ENABLED_DAY_OF_WEEK)
+                .build();
+
+        be = new Space.Builder()
+                .name(BE_NAME)
+                .color(BE_COLOR)
+                .map(luther)
+                .description(BE_DESCRIPTION)
+                .area(SPACE_DRAWING)
+                .setting(beSetting)
+                .build();
+
+        Setting feSetting = new Setting.Builder()
+                .availableStartTime(FE_AVAILABLE_START_TIME)
+                .availableEndTime(FE_AVAILABLE_END_TIME)
+                .reservationTimeUnit(FE_RESERVATION_TIME_UNIT)
+                .reservationMinimumTimeUnit(FE_RESERVATION_MINIMUM_TIME_UNIT)
+                .reservationMaximumTimeUnit(FE_RESERVATION_MAXIMUM_TIME_UNIT)
+                .reservationEnable(FE_RESERVATION_ENABLE)
+                .disabledWeekdays(FE_ENABLED_DAY_OF_WEEK)
+                .build();
+
+        fe = new Space.Builder()
+                .name(FE_NAME)
+                .color(FE_COLOR)
+                .map(luther)
+                .description(FE_DESCRIPTION)
+                .area(SPACE_DRAWING)
+                .setting(feSetting)
+                .build();
+
+        members.save(pobi);
+        maps.save(luther);
     }
 
     @DisplayName("공간을 저장한다.")
     @Test
     void save() {
         // given, when
-        Space savedSpace = spaces.save(BE);
+        Space savedSpace = spaces.save(be);
 
         // then
         assertThat(savedSpace.getId()).isNotNull();
-        assertThat(savedSpace).usingRecursiveComparison()
-                .isEqualTo(BE);
+        assertThat(savedSpace).isEqualTo(be);
     }
 
     @DisplayName("맵의 Id를 이용해 모든 공간을 찾아온다.")
     @Test
     void findAllByMapId() {
         // given
-        spaces.save(BE);
-        spaces.save(FE1);
+        spaces.save(be);
+        spaces.save(fe);
 
         // when
-        List<Space> actual = spaces.findAllByMapId(LUTHER.getId());
+        List<Space> actual = spaces.findAllByMapId(luther.getId());
 
         // then
-        assertThat(actual).usingRecursiveComparison()
-                .isEqualTo(List.of(BE, FE1));
+        assertThat(actual).containsExactlyInAnyOrderElementsOf(List.of(be, fe));
     }
 }
