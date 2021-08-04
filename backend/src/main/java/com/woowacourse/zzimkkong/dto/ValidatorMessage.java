cbf525529ccf99671e7ce79682a8eb6c343ffadf
y_woo :) <dusdn1702@naver.com>
2021-08-04 11:51:59 +0900

    refactor: 조직명 예외 메시지 수정

--- backend/src/main/java/com/woowacourse/zzimkkong/dto/ValidatorMessage.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/dto/ValidatorMessage.java
@@ -5,7 +5,7 @@ public class ValidatorMessage {
     public static final String EMAIL_MESSAGE = "올바른 이메일 형식이 아닙니다.";
     public static final String MEMBER_PASSWORD_MESSAGE = "비밀번호는 영어와 숫자를 포함해서 8자 이상 20자 이내로 입력해주세요.";
     public static final String RESERVATION_PASSWORD_MESSAGE = "비밀번호는 숫자 4자리만 입력 가능합니다.";
-    public static final String ORGANIZATION_MESSAGE = "조직명은 특수문자(-_!?.,)를 포함하여 20자 이내로 작성 가능합니다.";
+    public static final String ORGANIZATION_MESSAGE = "조직명은 특수문자 없이 20자 이내로 작성 가능합니다.";
     public static final String NAME_MESSAGE = "이름은 특수문자(-_!?.,)를 포함하여 20자 이내로 작성 가능합니다.";
     public static final String DESCRIPTION_MESSAGE = "예약 내용은 100자 이내로 작성 가능합니다.";
     public static final String FORMAT_MESSAGE = "날짜 및 시간 데이터 형식이 올바르지 않습니다.";
