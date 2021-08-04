99f093664b72f914a9bf017be7d7ff64de89760a
y_woo :) <dusdn1702@naver.com>
2021-08-04 13:58:28 +0900

    refactor: 120분 확인 완료

--- backend/src/main/java/com/woowacourse/zzimkkong/dto/TimeUnitValidator.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/dto/TimeUnitValidator.java
@@ -5,7 +5,7 @@ import javax.validation.ConstraintValidatorContext;
 import java.util.List;
 
 public class TimeUnitValidator implements ConstraintValidator<TimeUnit, Integer> {
-    private final List<Integer> TIME_UNITS = List.of(5, 10, 30, 60, 120);   //TODO 120분 해보고 테스트
+    private final List<Integer> TIME_UNITS = List.of(5, 10, 30, 60, 120);
 
     @Override
     public boolean isValid(Integer value, ConstraintValidatorContext context) {
