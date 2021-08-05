06fdf483b0ed5e24507363475ba57b3caf1bf104
Shim MunSeong <Puterism.K@gmail.com>
2021-08-05 13:38:42 +0900

    refactor: `useToggle` 커스텀 훅이 반환하는 배열에 `setValue` 추가

--- frontend/src/hooks/useToggle.ts
+++ frontend/src/hooks/useToggle.ts
@@ -1,13 +1,15 @@
-import { useState, ChangeEventHandler } from 'react';
+import { useState, ChangeEventHandler, Dispatch, SetStateAction } from 'react';
 
-const useToggle = (initialValue = false): [typeof value, ChangeEventHandler<HTMLInputElement>] => {
+const useToggle = (
+  initialValue = false
+): [typeof value, ChangeEventHandler<HTMLInputElement>, Dispatch<SetStateAction<typeof value>>] => {
   const [value, setValue] = useState(initialValue);
 
   const onChange: ChangeEventHandler<HTMLInputElement> = (event) => {
     setValue(event.target.checked);
   };
 
-  return [value, onChange];
+  return [value, onChange, setValue];
 };
 
 export default useToggle;
