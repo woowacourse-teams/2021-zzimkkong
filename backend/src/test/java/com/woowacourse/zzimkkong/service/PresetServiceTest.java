034b6ba56b95b75601436387cc871f30d95e7ca3
xrabcde <xrabcde@gmail.com>
2021-08-06 20:43:20 +0900

    test: 프리셋 전체조회 service test 작성

--- backend/src/test/java/com/woowacourse/zzimkkong/service/PresetServiceTest.java
+++ backend/src/test/java/com/woowacourse/zzimkkong/service/PresetServiceTest.java
@@ -4,6 +4,7 @@ import com.woowacourse.zzimkkong.domain.Member;
 import com.woowacourse.zzimkkong.domain.Preset;
 import com.woowacourse.zzimkkong.domain.Setting;
 import com.woowacourse.zzimkkong.dto.member.PresetCreateResponse;
+import com.woowacourse.zzimkkong.dto.member.PresetFindAllResponse;
 import com.woowacourse.zzimkkong.dto.space.SettingsRequest;
 import com.woowacourse.zzimkkong.repository.PresetRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,6 +12,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.List;
 
 import static com.woowacourse.zzimkkong.Constants.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -35,17 +38,15 @@ class PresetServiceTest extends ServiceTest {
     );
 
     private Member pobi;
+    private Setting setting;
+    private Preset firstPreset;
+    private Preset secondPreset;
 
     @BeforeEach
     void setUp() {
         pobi = new Member(1L, EMAIL, PASSWORD, ORGANIZATION);
-    }
 
-    @Test
-    @DisplayName("프리셋 저장 요청 시, 프리셋을 저장한다.")
-    void save() {
-        //given
-        Setting setting = new Setting.Builder()
+        setting = new Setting.Builder()
                 .availableStartTime(BE_AVAILABLE_START_TIME)
                 .availableEndTime(BE_AVAILABLE_END_TIME)
                 .reservationTimeUnit(BE_RESERVATION_TIME_UNIT)
@@ -55,15 +56,38 @@ class PresetServiceTest extends ServiceTest {
                 .enabledDayOfWeek(BE_ENABLED_DAY_OF_WEEK)
                 .build();
 
-        Preset expected = new Preset(1L, setting, pobi);
+        firstPreset = new Preset(1L, setting, pobi);
+        secondPreset = new Preset(2L, setting, pobi);
+    }
 
+    @Test
+    @DisplayName("프리셋 저장 요청 시, 프리셋을 저장한다.")
+    void save() {
+        //given
         given(presets.save(any(Preset.class)))
-                .willReturn(expected);
+                .willReturn(firstPreset);
 
         //when
         PresetCreateResponse presetCreateResponse = presetService.savePreset(settingsRequest, pobi);
 
         //then
         assertThat(presetCreateResponse.getId()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("모든 프리셋 조회 요청 시, member가 가진 모든 프리셋을 조회한다.")
+    void findAll() {
+        //given
+        List<Preset> expectedPresets = List.of(firstPreset, secondPreset);
+        given(presets.findAllByMember(any(Member.class)))
+                .willReturn(expectedPresets);
+
+        //when
+        PresetFindAllResponse presetFindAllResponse = presetService.findAllPresets(pobi);
+
+        //then
+        assertThat(presetFindAllResponse).usingRecursiveComparison()
+                .isEqualTo(PresetFindAllResponse.from(expectedPresets));
+
     }
 }
