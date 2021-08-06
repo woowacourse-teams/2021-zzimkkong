d6c5d5127a1df4028d9adfcb098a8ad292d185fe
xrabcde <xrabcde@gmail.com>
2021-08-06 10:22:09 +0900

    test: 예약자 공간전체조회 controller test 작성

---
+++ backend/src/test/java/com/woowacourse/zzimkkong/controller/GuestSpaceControllerTest.java
@@ -0,0 +1,111 @@
+package com.woowacourse.zzimkkong.controller;
+
+import com.woowacourse.zzimkkong.domain.Map;
+import com.woowacourse.zzimkkong.domain.Member;
+import com.woowacourse.zzimkkong.domain.Setting;
+import com.woowacourse.zzimkkong.domain.Space;
+import com.woowacourse.zzimkkong.dto.space.SpaceFindAllResponse;
+import com.woowacourse.zzimkkong.infrastructure.AuthorizationExtractor;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.util.List;
+
+import static com.woowacourse.zzimkkong.Constants.*;
+import static com.woowacourse.zzimkkong.Constants.SPACE_DRAWING;
+import static com.woowacourse.zzimkkong.DocumentUtils.*;
+import static com.woowacourse.zzimkkong.controller.ManagerSpaceControllerTest.saveSpace;
+import static com.woowacourse.zzimkkong.controller.MapControllerTest.saveMap;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
+
+public class GuestSpaceControllerTest extends AcceptanceTest {
+    private String spaceApi;
+    private Space be;
+    private Space fe;
+
+    @BeforeEach
+    void setUp() {
+        String lutherId = saveMap("/api/managers/maps", mapCreateUpdateRequest).header("location").split("/")[4];
+        spaceApi = "/api/managers/maps/" + lutherId + "/spaces";
+        ExtractableResponse<Response> saveBeSpaceResponse = saveSpace(spaceApi, beSpaceCreateUpdateRequest);
+        ExtractableResponse<Response> saveFe1SpaceResponse = saveSpace(spaceApi, feSpaceCreateUpdateRequest);
+
+        Long beSpaceId = Long.valueOf(saveBeSpaceResponse.header("location").split("/")[6]);
+        Long feSpaceId = Long.valueOf(saveFe1SpaceResponse.header("location").split("/")[6]);
+
+        Member pobi = new Member(EMAIL, PASSWORD, ORGANIZATION);
+        Map luther = new Map(LUTHER_NAME, MAP_DRAWING_DATA, MAP_IMAGE_URL, pobi);
+        Setting beSetting = new Setting.Builder()
+                .availableStartTime(BE_AVAILABLE_START_TIME)
+                .availableEndTime(BE_AVAILABLE_END_TIME)
+                .reservationTimeUnit(BE_RESERVATION_TIME_UNIT)
+                .reservationMinimumTimeUnit(BE_RESERVATION_MINIMUM_TIME_UNIT)
+                .reservationMaximumTimeUnit(BE_RESERVATION_MAXIMUM_TIME_UNIT)
+                .reservationEnable(BE_RESERVATION_ENABLE)
+                .enabledDayOfWeek(BE_ENABLED_DAY_OF_WEEK)
+                .build();
+
+        Setting feSetting = new Setting.Builder()
+                .availableStartTime(FE_AVAILABLE_START_TIME)
+                .availableEndTime(FE_AVAILABLE_END_TIME)
+                .reservationTimeUnit(FE_RESERVATION_TIME_UNIT)
+                .reservationMinimumTimeUnit(FE_RESERVATION_MINIMUM_TIME_UNIT)
+                .reservationMaximumTimeUnit(FE_RESERVATION_MAXIMUM_TIME_UNIT)
+                .reservationEnable(FE_RESERVATION_ENABLE)
+                .enabledDayOfWeek(FE_ENABLED_DAY_OF_WEEK)
+                .build();
+
+        be = new Space.Builder()
+                .id(beSpaceId)
+                .name(BE_NAME)
+                .color(BE_COLOR)
+                .map(luther)
+                .description(BE_DESCRIPTION)
+                .area(SPACE_DRAWING)
+                .setting(beSetting)
+                .build();
+
+        fe = new Space.Builder()
+                .id(feSpaceId)
+                .name(FE_NAME)
+                .color(FE_COLOR)
+                .map(luther)
+                .description(FE_DESCRIPTION)
+                .area(SPACE_DRAWING)
+                .setting(feSetting)
+                .build();
+    }
+
+    @Test
+    @DisplayName("전체 공간에 대한 정보를 조회한다.")
+    void findAll() {
+        // given, when
+        ExtractableResponse<Response> response = findAllSpace(spaceApi);
+        SpaceFindAllResponse actual = response.body().as(SpaceFindAllResponse.class);
+        SpaceFindAllResponse expected = SpaceFindAllResponse.from(List.of(be, fe));
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(actual).usingRecursiveComparison()
+                .ignoringExpectedNullFields()
+                .isEqualTo(expected);
+    }
+
+    private ExtractableResponse<Response> findAllSpace(final String api) {
+        return RestAssured
+                .given(getRequestSpecification()).log().all()
+                .accept("application/json")
+                .header("Authorization", AuthorizationExtractor.AUTHENTICATION_TYPE + " " + accessToken)
+                .filter(document("space/getAll", getRequestPreprocessor(), getResponsePreprocessor()))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().get(api)
+                .then().log().all().extract();
+    }
+}
