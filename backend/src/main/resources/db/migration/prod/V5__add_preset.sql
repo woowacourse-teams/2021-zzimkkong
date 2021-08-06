186ef3a9e5621ab13cf99263fc56a61551c73ee5
y_woo :) <dusdn1702@naver.com>
2021-08-06 11:49:41 +0900

    docs: preset 추가 sql문 작성

---
+++ backend/src/main/resources/db/migration/prod/V5__add_preset.sql
@@ -0,0 +1,13 @@
+create table preset
+(
+    id                            bigint       not null auto_increment primary key,
+    available_start_time          time         not null,
+    available_end_time            time         not null,
+    reservation_time_unit         integer      not null,
+    reservation_minimum_time_unit integer      not null,
+    reservation_maximum_time_unit integer      not null,
+    reservation_enable            bit(1)       not null,
+    enabled_day_of_week           varchar(255),
+    manager_id                    bigint       not null,
+    foreign key (manager_id) references member (id)
+);
