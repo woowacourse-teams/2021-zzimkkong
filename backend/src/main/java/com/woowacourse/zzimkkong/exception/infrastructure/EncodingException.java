f1693532df10a30e1c9ce5055cbd92d1c58d7228
Kimun Kim <kkm97351@gmail.com>
2021-08-07 18:56:14 +0900

    refactor: Infrastructure 관련 exception 재계층화 및 중복 제거

--- backend/src/main/java/com/woowacourse/zzimkkong/exception/infrastructure/EncodingException.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/exception/infrastructure/EncodingException.java
@@ -2,10 +2,10 @@ package com.woowacourse.zzimkkong.exception.infrastructure;
 
 import org.springframework.http.HttpStatus;
 
-public class EncodingException extends InfrastructureException {
+public class EncodingException extends SharingIdException {
     private static final String MESSAGE = "링크 생성에 실패했습니다.";
 
     public EncodingException(Throwable throwable) {
-        super(MESSAGE, throwable, HttpStatus.INTERNAL_SERVER_ERROR, "publicId");
+        super(MESSAGE, throwable, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
--- backend/src/main/java/com/woowacourse/zzimkkong/exception/infrastructure/InsufficientSecretKeyLengthException.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/exception/infrastructure/InsufficientSecretKeyLengthException.java
@@ -2,11 +2,11 @@ package com.woowacourse.zzimkkong.exception.infrastructure;
 
 import org.springframework.http.HttpStatus;
 
-public class InsufficientSecretKeyLengthException extends InfrastructureException {
-    private static final String FIELD = "config/aes256Transcoder.properties";
-    private static final String MESSAGE = "공유링크 생성시 사용되는 암호화 키값의 길이가 충분하지 못합니다. 관리자는 설정 파일의 키값을 재설정한 후 다시 서버를 구동해주시기 바랍니다.";
+public class InsufficientSecretKeyLengthException extends SharingIdException {
+    private static final String MESSAGE = "공유링크 생성시 사용되는 암호화 키값의 길이(32자 이상)가 충분하지 못합니다. " +
+            "관리자는 설정 파일(config/aes256Transcoder.properties)의 키값을 재설정한 후 다시 서버를 구동해주시기 바랍니다.";
 
     public InsufficientSecretKeyLengthException() {
-        super(MESSAGE, HttpStatus.INTERNAL_SERVER_ERROR, FIELD);
+        super(MESSAGE, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
