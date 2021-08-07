15fdb1e0155d2b2a4519b73aed7e42a4551112e1
Kimun Kim <kkm97351@gmail.com>
2021-08-07 12:07:47 +0900

    refactor: En/Decoding Exception 메세지 변경

--- backend/src/main/java/com/woowacourse/zzimkkong/exception/infrastructure/DecodingException.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/exception/infrastructure/DecodingException.java
@@ -3,9 +3,9 @@ package com.woowacourse.zzimkkong.exception.infrastructure;
 import org.springframework.http.HttpStatus;
 
 public class DecodingException extends InfrastructureException {
-    private static final String MESSAGE = "접근을 위한 키의 디코딩에 실패했습니다. 관리자에게 문의하세요.";
+    private static final String MESSAGE = "링크 접근에 실패했습니다.";
 
     public DecodingException(Throwable throwable) {
-        super(MESSAGE, throwable, HttpStatus.INTERNAL_SERVER_ERROR, "publicMapId");
+        super(MESSAGE, throwable, HttpStatus.INTERNAL_SERVER_ERROR, "publicId");
     }
 }
--- backend/src/main/java/com/woowacourse/zzimkkong/exception/infrastructure/EncodingException.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/exception/infrastructure/EncodingException.java
@@ -3,9 +3,9 @@ package com.woowacourse.zzimkkong.exception.infrastructure;
 import org.springframework.http.HttpStatus;
 
 public class EncodingException extends InfrastructureException {
-    private static final String MESSAGE = "접근을 위한 키의 인코딩에 실패했습니다. 관리자에게 문의하세요.";
+    private static final String MESSAGE = "링크 생성에 실패했습니다.";
 
     public EncodingException(Throwable throwable) {
-        super(MESSAGE, throwable, HttpStatus.INTERNAL_SERVER_ERROR, "publicMapId");
+        super(MESSAGE, throwable, HttpStatus.INTERNAL_SERVER_ERROR, "publicId");
     }
 }
