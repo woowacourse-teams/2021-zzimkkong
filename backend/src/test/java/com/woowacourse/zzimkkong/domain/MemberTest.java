4ca5a502f424c31f86260b85d8926a5c3f3bce16
y_woo :) <dusdn1702@naver.com>
2021-08-09 16:24:03 +0900

    refactor: memberTest findPreset 보강

--- backend/src/test/java/com/woowacourse/zzimkkong/domain/MemberTest.java
+++ backend/src/test/java/com/woowacourse/zzimkkong/domain/MemberTest.java
@@ -3,6 +3,8 @@ package com.woowacourse.zzimkkong.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
 import static com.woowacourse.zzimkkong.Constants.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -30,7 +32,7 @@ class MemberTest {
     @Test
     @DisplayName("자신의 preset 중에 id가 같은 것을 반환한다.")
     void findPresetById() {
-        //given
+        //given, when
         Member member = new Member(1L, EMAIL, PASSWORD, ORGANIZATION);
         Setting setting = new Setting.Builder()
                 .availableStartTime(BE_AVAILABLE_START_TIME)
@@ -43,10 +45,8 @@ class MemberTest {
                 .build();
         Preset preset = new Preset(1L, PRESET_NAME1, setting, member);
 
-        //when
-        member.addPreset(preset);
-
         //then
-        assertThat(member.findPresetById(1L).isPresent()).isTrue();
+        assertThat(member.findPresetById(1L))
+                .isEqualTo(Optional.of(preset));
     }
 }
