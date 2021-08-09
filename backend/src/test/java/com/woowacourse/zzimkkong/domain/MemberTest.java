41537af10eaa6cdd032d8d563bfb0aaa8b6b3d91
Yeonwoo Cho <dusdn1702@gmail.com>
2021-08-09 16:51:30 +0900

    feat: 프리셋을 저장한다. 프리셋을 삭제한다. (#344)
    * feat: PresetService save 기능과 테스트 구현
    
    * feat: PresetRepositoryTest 작성
    
    * feat: PresetController create 기능 구현, 테스트 작성
    
    * docs: preset 추가 sql문 작성
    
    * refactor: member와 preset ManyToOne 단방향 관계 설정
    
    * refactor: member와 preset 양방향 관계 설정
    
    * docs: 프리셋 생성 api 문서 추가
    
    * refactor: transactional 추가
    
    * refactor: 피드백 반영
    
    * refactor: 프리셋에 이름 추가
    
    * docs: 프리셋 이름 추가에 따른 sql 컬럼 추가
    
    * feat: 프리셋 삭제 service layer 구현
    
    * feat: 프리셋 삭제 controller layer 구현
    
    * docs: 프리셋 삭제 api 문서 추가
    
    * refactor: PresetCreateRequest 패키지 이동, final 추가
    
    * refactor: delete 관련 피드백 반영
    
    * refactor: 편의 메서드 추가
    
    * refactor: preset 생성 시 member에 presets add
    
    * refactor: deletePreset 예외 테스트, hasSameId 테스트 추가
    
    * refactor: memberTest findPreset 보강

--- backend/src/test/java/com/woowacourse/zzimkkong/domain/MemberTest.java
+++ backend/src/test/java/com/woowacourse/zzimkkong/domain/MemberTest.java
@@ -3,6 +3,8 @@ package com.woowacourse.zzimkkong.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
 import static com.woowacourse.zzimkkong.Constants.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,16 +12,41 @@ class MemberTest {
     @Test
     @DisplayName("비밀번호가 일치하면 true를 반환한다.")
     void checkPassword() {
+        //given
         Member member = new Member(EMAIL, PASSWORD, ORGANIZATION);
 
+        //when, then
         assertThat(member.checkPassword(PASSWORD)).isTrue();
     }
 
     @Test
     @DisplayName("비밀번호가 일치하지 않으면 false를 반환한다.")
     void checkWrongPassword() {
+        //given
         Member member = new Member(EMAIL, PASSWORD, ORGANIZATION);
 
+        //when, then
         assertThat(member.checkPassword(PASSWORD + "%")).isFalse();
+    }
+
+    @Test
+    @DisplayName("자신의 preset 중에 id가 같은 것을 반환한다.")
+    void findPresetById() {
+        //given, when
+        Member member = new Member(1L, EMAIL, PASSWORD, ORGANIZATION);
+        Setting setting = new Setting.Builder()
+                .availableStartTime(BE_AVAILABLE_START_TIME)
+                .availableEndTime(BE_AVAILABLE_END_TIME)
+                .reservationTimeUnit(BE_RESERVATION_TIME_UNIT)
+                .reservationMinimumTimeUnit(BE_RESERVATION_MINIMUM_TIME_UNIT)
+                .reservationMaximumTimeUnit(BE_RESERVATION_MAXIMUM_TIME_UNIT)
+                .reservationEnable(BE_RESERVATION_ENABLE)
+                .enabledDayOfWeek(BE_ENABLED_DAY_OF_WEEK)
+                .build();
+        Preset preset = new Preset(1L, PRESET_NAME1, setting, member);
+
+        //then
+        assertThat(member.findPresetById(1L))
+                .isEqualTo(Optional.of(preset));
     }
 }
