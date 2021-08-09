3519b2ae0867dffb6ecd85c10b3390970ddb9ac2
Sunny.K <sunnyk5780@gmail.com>
2021-08-09 11:24:17 +0900

    feat: Header 로고 선택 시 메인 화면으로 이동

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,15 +1,24 @@
+import { Link, useParams } from 'react-router-dom';
 import { ReactComponent as LogoIcon } from 'assets/svg/logo.svg';
+import PATH from 'constants/path';
 import * as Styled from './Header.styles';
 
-const Header = (): JSX.Element => (
-  <Styled.Header>
-    <Styled.HeaderLayout>
-      <Styled.Logo>
-        <LogoIcon />
-      </Styled.Logo>
-      <Styled.Title>찜꽁</Styled.Title>
-    </Styled.HeaderLayout>
-  </Styled.Header>
-);
+const Header = (): JSX.Element => {
+  const params = useParams<{ publicMapId: string }>();
+  const publicMapId = params?.publicMapId;
+
+  return (
+    <Styled.Header>
+      <Styled.HeaderLayout>
+        <Styled.HeaderLink to={publicMapId ? `/guest/${publicMapId}` : PATH.MANAGER_MAIN}>
+          <Styled.Logo>
+            <LogoIcon />
+          </Styled.Logo>
+          <Styled.Title>찜꽁</Styled.Title>
+        </Styled.HeaderLink>
+      </Styled.HeaderLayout>
+    </Styled.Header>
+  );
+};
 
 export default Header;
