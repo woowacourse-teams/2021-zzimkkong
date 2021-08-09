5634df1ca06fdbeb670ddd2438a786da60d46856
Sunny.K <sunnyk5780@gmail.com>
2021-08-09 11:33:52 +0900

    refactor: Params 타입선언 분리

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,10 +1,14 @@
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { ReactComponent as LogoIcon } from 'assets/svg/logo.svg';
 import PATH from 'constants/path';
 import * as Styled from './Header.styles';
 
+interface Params {
+  publicMapId?: string;
+}
+
 const Header = (): JSX.Element => {
-  const params = useParams<{ publicMapId: string }>();
+  const params = useParams<Params>();
   const publicMapId = params?.publicMapId;
 
   return (
