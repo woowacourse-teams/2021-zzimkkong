9dc6f9eee9f0e1d4f0d8ba93265b4ff4a4893c49
Kimun Kim <kkm97351@gmail.com>
2021-08-09 15:55:01 +0900

    refactor: PUBLIC_MAP_ID -> SHARING_MAP_ID 로 상수명 변경

--- backend/src/main/java/com/woowacourse/zzimkkong/exception/infrastructure/EncodingException.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/exception/infrastructure/EncodingException.java
@@ -6,6 +6,6 @@ public class EncodingException extends InfrastructureException {
     private static final String MESSAGE = "링크 생성에 실패했습니다.";
 
     public EncodingException(Throwable throwable) {
-        super(MESSAGE, throwable, HttpStatus.INTERNAL_SERVER_ERROR, PUBLIC_MAP_ID);
+        super(MESSAGE, throwable, HttpStatus.INTERNAL_SERVER_ERROR, SHARING_MAP_ID);
     }
 }
