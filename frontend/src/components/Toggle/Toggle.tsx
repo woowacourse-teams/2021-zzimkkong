f7321d22874960bb6e830272efedf85ecda0dfc3
Shim MunSeong <Puterism.K@gmail.com>
2021-08-10 15:26:23 +0900

    feat: `Toggle` 컴포넌트 및 `useToggle` 커스텀 훅 구현

---
+++ frontend/src/components/Toggle/Toggle.tsx
@@ -0,0 +1,35 @@
+import { InputHTMLAttributes } from 'react';
+import * as Styled from './Toggle.styles';
+
+export interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  variant?: 'default' | 'primary';
+  text: string;
+  checkedText: string;
+  textPosition?: 'left' | 'right';
+}
+
+const Toggle = ({
+  variant = 'default',
+  text,
+  checkedText,
+  checked,
+  textPosition = 'right',
+  ...props
+}: Props): JSX.Element => {
+  return (
+    <Styled.Label>
+      {textPosition === 'left' && (
+        <Styled.LabelText>{checked ? checkedText : text}</Styled.LabelText>
+      )}
+      <Styled.Toggle>
+        <Styled.Input type="checkbox" variant={variant} {...props} />
+        <Styled.Slider />
+      </Styled.Toggle>
+      {textPosition === 'right' && (
+        <Styled.LabelText>{checked ? checkedText : text}</Styled.LabelText>
+      )}
+    </Styled.Label>
+  );
+};
+
+export default Toggle;
