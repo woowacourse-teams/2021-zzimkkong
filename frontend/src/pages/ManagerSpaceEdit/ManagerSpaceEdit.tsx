8024637266968e6f2f89984f5cf5b4997ed31c7d
Shim MunSeong <Puterism.K@gmail.com>
2021-08-12 22:21:12 +0900

    fix: 공간 편집 시, 썸네일에 공간 영역이 올바르게 반영되지 않는 문제 수정

--- frontend/src/pages/ManagerSpaceEdit/ManagerSpaceEdit.tsx
+++ frontend/src/pages/ManagerSpaceEdit/ManagerSpaceEdit.tsx
@@ -587,6 +587,11 @@ const ManagerSpaceEdit = (): JSX.Element => {
         end: formatTimeWithSecond(new Date(`${todayDate}T${availableEndTime}`)),
       };
 
+      const targetSpaces =
+        selectedSpaceId && !isAddingSpace
+          ? spaces.filter(({ id }) => selectedSpaceId !== id)
+          : spaces;
+
       const mapImageSvg = `
     <svg
       xmlns='http://www.w3.org/2000/svg'
@@ -612,8 +617,7 @@ const ManagerSpaceEdit = (): JSX.Element => {
         : ''
     }
 
-      ${spaces
-        .filter(({ id }) => selectedSpaceId !== id)
+      ${targetSpaces
         .map(
           ({ color, area }) => `
             <g>
