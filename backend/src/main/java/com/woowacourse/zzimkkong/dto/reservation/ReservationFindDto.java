89b97f55e9b395ebcd24994b7d7701fcf5d49ec6
sakjung <ssangyu123@gmail.com>
2021-08-12 11:28:26 +0900

    refactor: ReservaitonFindDto 개행 통일

--- backend/src/main/java/com/woowacourse/zzimkkong/dto/reservation/ReservationFindDto.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/dto/reservation/ReservationFindDto.java
@@ -11,16 +11,27 @@ import java.time.LocalDate;
 public class ReservationFindDto extends ReservationFindAllDto {
     private Long spaceId;
 
-    private ReservationFindDto(final Long mapId, final Long spaceId, final LocalDate date, final Member manager) {
+    private ReservationFindDto(
+            final Long mapId,
+            final Long spaceId,
+            final LocalDate date,
+            final Member manager) {
         super(mapId, date, manager);
         this.spaceId = spaceId;
     }
 
-    public static ReservationFindDto of(final Long mapId, final Long spaceId, final LocalDate date) {
+    public static ReservationFindDto of(
+            final Long mapId,
+            final Long spaceId,
+            final LocalDate date) {
         return new ReservationFindDto(mapId, spaceId, date, new Member());
     }
 
-    public static ReservationFindDto of(final Long mapId, final Long spaceId, final LocalDate date, final Member manager) {
+    public static ReservationFindDto of(
+            final Long mapId,
+            final Long spaceId,
+            final LocalDate date,
+            final Member manager) {
         return new ReservationFindDto(mapId, spaceId, date, manager);
     }
 }
