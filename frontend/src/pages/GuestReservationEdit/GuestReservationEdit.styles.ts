1fd293b073a9d2789eb44c1f1eeccbdefcd0d623
Shim MunSeong <Puterism.K@gmail.com>
2021-08-12 21:37:49 +0900

    chore: 누락된 스타일 컴포넌트 수정

--- frontend/src/pages/GuestReservationEdit/GuestReservationEdit.styles.ts
+++ frontend/src/pages/GuestReservationEdit/GuestReservationEdit.styles.ts
@@ -9,6 +9,12 @@ export const ReservationForm = styled.form`
   margin: 1.5rem 0 5rem 0;
 `;
 
+export const PageHeader = styled.h2`
+  font-size: 1.625rem;
+  font-weight: 700;
+  margin: 1.5rem 0;
+`;
+
 export const Section = styled.section`
   margin: 1.5rem 0;
 `;
