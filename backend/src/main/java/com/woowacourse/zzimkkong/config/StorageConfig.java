740b367759edac1cb94e19647d8f3ead22707e58
Kimun Kim <kkm97351@gmail.com>
2021-08-31 14:35:15 +0900

    refactor: config 클래스 패키지로 분류

--- backend/src/main/java/com/woowacourse/zzimkkong/config/StorageConfig.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/config/StorageConfig.java
@@ -1,4 +1,4 @@
-package com.woowacourse.zzimkkong;
+package com.woowacourse.zzimkkong.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
