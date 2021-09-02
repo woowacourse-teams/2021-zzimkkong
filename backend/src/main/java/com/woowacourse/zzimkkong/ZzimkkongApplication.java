9ac3d8366a265ea4f06b8259ef6f9d894193bde7
sakjung <ssangyu123@gmail.com>
2021-09-02 14:35:58 +0900

    feat: Application에 타임존 설정 로직 추가

--- backend/src/main/java/com/woowacourse/zzimkkong/ZzimkkongApplication.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/ZzimkkongApplication.java
@@ -3,8 +3,16 @@ package com.woowacourse.zzimkkong;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class ZzimkkongApplication {
+    @PostConstruct
+    private void setTimeZone(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(ZzimkkongApplication.class, args);
     }
