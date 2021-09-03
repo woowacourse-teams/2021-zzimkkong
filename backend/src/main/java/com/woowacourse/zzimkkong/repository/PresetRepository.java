f06f88352eaff06ef8ac7261283af99d958c17bb
sakjung <ssangyu123@gmail.com>
2021-09-03 18:20:08 +0900

    refactor: 코드 냄새 제거

--- backend/src/main/java/com/woowacourse/zzimkkong/repository/PresetRepository.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/repository/PresetRepository.java
@@ -3,7 +3,5 @@ package com.woowacourse.zzimkkong.repository;
 import com.woowacourse.zzimkkong.domain.Preset;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface PresetRepository extends JpaRepository<Preset, Long> {
 }
