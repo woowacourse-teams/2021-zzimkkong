64801c5af47e9d60e3e24cb40d409766d397af34
xrabcde <xrabcde@gmail.com>
2021-09-03 18:34:06 +0900

    refactor: 코드 냄새 제거(젠킨스 촉발) (#495)
    * refactor: 코드 냄새 제거
    
    * refactor: 코드 냄새 제거
    
    * refactor: 코드 냄새 제거
    
    * refactor: 코드 냄새 제거
    
    * refactor: 코드 냄새 제거
    
    * refactor: 코드 냄새 제거
    
    Co-authored-by: sakjung <ssangyu123@gmail.com>

--- backend/src/main/java/com/woowacourse/zzimkkong/service/ReservationService.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/service/ReservationService.java
@@ -102,9 +102,9 @@ public class ReservationService {
         LocalDate date = reservationFindDto.getDate();
         Space space = map.findSpaceById(spaceId)
                 .orElseThrow(NoSuchSpaceException::new);
-        List<Reservation> reservations = getReservations(Collections.singletonList(space), date);
+        List<Reservation> findReservations = getReservations(Collections.singletonList(space), date);
 
-        return ReservationFindResponse.from(reservations);
+        return ReservationFindResponse.from(findReservations);
     }
 
     @Transactional(readOnly = true)
