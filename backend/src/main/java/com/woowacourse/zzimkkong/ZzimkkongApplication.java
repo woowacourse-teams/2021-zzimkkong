706f4cc6510c8cc8337b5665e1c05b607acfff35
sakjung <ssangyu123@gmail.com>
2021-09-04 23:12:04 +0900

    chore: 젠킨스 촉발

--- backend/src/main/java/com/woowacourse/zzimkkong/ZzimkkongApplication.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/ZzimkkongApplication.java
@@ -14,7 +14,7 @@ public class ZzimkkongApplication {
     }
 
     public static void main(String[] args) {
-        // jenkins invoke invoke invoke
+        // jenkins invoke invoke
         SpringApplication.run(ZzimkkongApplication.class, args);
     }
 }
