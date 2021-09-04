ee094c03dabe8670f32337cdd829b42efef8855e
xrabcde <xrabcde@gmail.com>
2021-09-04 16:46:07 +0900

    feat: front dev 젠킨스 파이프라인을 생성한다 (#502)
    * refactor: 코드 냄새 제거
    
    * chore: 젠킨스 촉발
    
    * chore: 젠킨스 촉발
    
    * chore: 젠킨스 촉발
    
    Co-authored-by: sakjung <ssangyu123@gmail.com>

--- backend/src/main/java/com/woowacourse/zzimkkong/service/strategy/ExcludeReservationCreateStrategy.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/service/strategy/ExcludeReservationCreateStrategy.java
@@ -8,6 +8,6 @@ import java.util.List;
 public class ExcludeReservationCreateStrategy implements ExcludeReservationStrategy {
     @Override
     public void apply(final Space space, final List<Reservation> reservationsOnDate) {
-        // 예약 생성 시는 검증 전 예약을 제외하지 않아도 되므로 생략
+        // 예약 생성 시는 검증 전 예약을 제외하지 않아도 되므로 생략합니다
     }
 }
