1593ee050106dc75c17f6fae67d3bed8e2d2fcfd
y_woo :) <dusdn1702@naver.com>
2021-09-07 23:18:45 +0900

    refactor: google 사용하지 않는 응답 삭제

--- backend/src/main/java/com/woowacourse/zzimkkong/infrastructure/oauth/dto/GoogleTokenResponse.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/infrastructure/oauth/dto/GoogleTokenResponse.java
@@ -10,26 +10,15 @@ public class GoogleTokenResponse {
     @JsonProperty("access_token")
     private String accessToken;
 
-    private String scope;
-
-    @JsonProperty("token_type")
-    private String tokenType;
-
     @JsonProperty("error")
     String error;
 
     @JsonProperty("error_description")
     String errorDescription;
 
-    @JsonProperty("error_uri")
-    String errorUri;
-
-    public GoogleTokenResponse(String accessToken, String scope, String tokenType, String error, String errorDescription, String errorUri) {
+    public GoogleTokenResponse(String accessToken, String error, String errorDescription) {
         this.accessToken = accessToken;
-        this.scope = scope;
-        this.tokenType = tokenType;
         this.error = error;
         this.errorDescription = errorDescription;
-        this.errorUri = errorUri;
     }
 }
