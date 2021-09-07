4a100fcb305808e2f12227dabe3cefffe8ce364d
y_woo :) <dusdn1702@naver.com>
2021-09-07 22:57:06 +0900

    refactor: google 로그인 구현체 정리

---
+++ backend/src/main/java/com/woowacourse/zzimkkong/domain/oauth/GoogleUserInfo.java
@@ -0,0 +1,50 @@
+package com.woowacourse.zzimkkong.domain.oauth;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class GoogleUserInfo implements OAuthUserInfo {
+    @JsonProperty("id")
+    private String id;
+
+    @JsonProperty("email")
+    private String email;
+
+    @JsonProperty("verified_email")
+    private String verifiedEmail;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("given_name")
+    private String givenName;
+
+    @JsonProperty("family_name")
+    private String familyName;
+
+    @JsonProperty("picture")
+    private String picture;
+
+    @JsonProperty("locale")
+    private String locale;
+
+    public GoogleUserInfo(String id, String email, String verifiedEmail, String name, String givenName, String familyName, String picture, String locale) {
+        this.id = id;
+        this.email = email;
+        this.verifiedEmail = verifiedEmail;
+        this.name = name;
+        this.givenName = givenName;
+        this.familyName = familyName;
+        this.picture = picture;
+        this.locale = locale;
+    }
+
+    @Override
+    public String getEmail() {
+        return email;
+    }
+}
+
--- backend/src/main/java/com/woowacourse/zzimkkong/infrastructure/oauth/GoogleRequester.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/infrastructure/oauth/GoogleRequester.java
@@ -1,19 +1,15 @@
 package com.woowacourse.zzimkkong.infrastructure.oauth;
 
 import com.woowacourse.zzimkkong.domain.OAuthProvider;
+import com.woowacourse.zzimkkong.domain.oauth.GoogleUserInfo;
 import com.woowacourse.zzimkkong.domain.oauth.OAuthUserInfo;
+import com.woowacourse.zzimkkong.exception.infrastructure.oauth.UnableToGetTokenResponseFromGoogleException;
 import com.woowacourse.zzimkkong.infrastructure.oauth.dto.GoogleTokenResponse;
-import com.woowacourse.zzimkkong.infrastructure.oauth.dto.GoogleUserResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Collections;
@@ -31,7 +27,7 @@ public class GoogleRequester implements OAuthAPIRequester {
             @Value("${google.secret-id}")
                     String secretId,
             @Value("${google.redirect-uri}")
-            String redirectUri) {
+                    String redirectUri) {
         this.clientId = clientId;
         this.secretId = secretId;
         this.redirectUri = redirectUri;
@@ -49,59 +45,36 @@ public class GoogleRequester implements OAuthAPIRequester {
     }
 
     private String getToken(String code) {
-        OAuthProvider oauthProvider = OAuthProvider.GOOGLE;
-        GoogleTokenResponse googleTokenResponse = WebClient.create()
+        GoogleTokenResponse googleTokenResponse = WebClient.builder()
+                .baseUrl("https://www.googleapis.com/oauth2/v4/token")
+                .build()
                 .post()
-                .uri("https://www.googleapis.com/oauth2/v4/token")
+                .uri(uriBuilder -> uriBuilder
+                        .queryParam("code", code)
+                        .queryParam("client_id", clientId)
+                        .queryParam("client_secret", secretId)
+                        .queryParam("redirect_uri", redirectUri)
+                        .queryParam("grant_type", "authorization_code")
+                        .build())
                 .headers(header -> {
-                    header.setBasicAuth(clientId, secretId);
-                    header.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
                     header.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
                     header.setAcceptCharset(Collections.singletonList(StandardCharsets.UTF_8));
                 })
-                .bodyValue(tokenRequest(code))
                 .retrieve()
                 .bodyToMono(GoogleTokenResponse.class)
-                .block();
-//        Mono<ResponseEntity<GoogleTokenResponse>> googleTokenResponse = WebClient.create()
-//                .post()
-//                .uri(uri -> uri.path(oauthProvider.getTokenUri())
-//                        .queryParam("code", code)
-//                        .queryParam("client_id", clientId)
-//                        .queryParam("client_secret", secretId)
-//                        .queryParam("redirect_uri", redirectUri)
-//                        .queryParam("grant_type", "authorization_code")
-//                        .build())
-//                .retrieve()
-//                .onStatus(HttpStatus::is4xxClientError, clientResponse -> Mono.error(new RuntimeException(clientResponse.toString())))
-//                .onStatus(HttpStatus::is5xxServerError, clientResponse -> Mono.error(new RuntimeException(clientResponse.toString())))
-//                .toEntity(GoogleTokenResponse.class);
-//        ResponseEntity<GoogleTokenResponse> s = googleTokenResponse.block();
-//        GoogleTokenResponse body = s.getBody();
-//        String accessToken = body.getAccessToken();
-
+                .blockOptional()
+                .orElseThrow(UnableToGetTokenResponseFromGoogleException::new);
         return googleTokenResponse.getAccessToken();
     }
 
-    private MultiValueMap<String, String> tokenRequest(String code) {
-        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
-        formData.add("code", code);
-        formData.add("grant_type", "authorization_code");
-        formData.add("redirect_uri", redirectUri);
-        return formData;
-    }
-
-    private OAuthUserInfo getUserInfo(String token) {
-        //todo 샐리
-        return null;
-        //"https://www.googleapis.com/oauth2/v2/userinfo"
-
-//        return WebClient.create()
-//                .get()
-//                .uri(OAuthProvider.GOOGLE.getUserApi())
-//                .headers(httpHeaders -> httpHeaders.setBearerAuth(token))
-//                .retrieve()
-//                .bodyToMono(GoogleUserResponse.class)
-//                .block();
+    private GoogleUserInfo getUserInfo(String token) {
+        return WebClient.create()
+                .get()
+                .uri("https://www.googleapis.com/oauth2/v2/userinfo")
+                .headers(httpHeaders -> httpHeaders.setBearerAuth(token))
+                .retrieve()
+                .bodyToMono(GoogleUserInfo.class)
+                .blockOptional()
+                .orElseThrow(UnableToGetTokenResponseFromGoogleException::new);
     }
 }
