3e2cec21572fbd820a1d58f32fa88fceb5a3e893
Kimun Kim <kkm97351@gmail.com>
2021-09-08 17:19:29 +0900

    feat: OAuth 로그인시 OAuth 제공사가 일치하는지 검증

---
+++ backend/src/main/java/com/woowacourse/zzimkkong/exception/authorization/OAuthProviderMismatchException.java
@@ -0,0 +1,12 @@
+package com.woowacourse.zzimkkong.exception.authorization;
+
+import com.woowacourse.zzimkkong.exception.ZzimkkongException;
+import org.springframework.http.HttpStatus;
+
+public class OAuthProviderMismatchException extends ZzimkkongException {
+    private static final String MESSAGE = "소셜 로그인 제공자가 다릅니다. 다른 소셜 로그인 제공 업체를 통해 로그인하세요.";
+
+    public OAuthProviderMismatchException() {
+        super(MESSAGE, HttpStatus.UNAUTHORIZED);
+    }
+}
