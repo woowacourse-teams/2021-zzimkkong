dffd50821dd229755e37bdfbab3a59add2f31349
JO YUN HO <yujo.dev@gmail.com>
2021-09-09 16:12:31 +0900

    refactor: GuestReservation과 GuestReservationEdit 컴포넌트 통합 (#518)
    * refactor: 예약자 예약 관리 기능을 하나의 컴포넌트에서 하도록 통합
    
    - GuestReservationEdit 컴포넌트를 GuestReservation 컴포넌트에 병합
    
    * refactor: 코드 정리 및 불필요한 컴포넌트 제거
    
    * refactor: useInput으로 관리하던 상태들을 useInputs로 관리하도록 변경
    
    * refactor: ReservationForm 컴포넌트 분리
    
    * refactor: 매직 넘버 상수화
    
    * refactor: window scroll 초기화 기능 Custom Hook으로 분리
    
    * refactor: Form과 관련 없는 요소들 분리
    
    * refactor: 현재 모드를 판단할 수 있는 식별자를 추가
    
    * refactor: 시간 관련 상수 별도의 파일로 분리
    
    * refactor: GuestReservation 컴포넌트와 ReservationForm 컴포넌트의 관심사 분리
    
    * refactor: 매직 넘버 상수로 분리
    
    * refactor: date를 props로 전달하도록 변경
    
    * refactor: 매직넘버 상수화
    
    * refactor: Event Type 컨벤션 통일
    
    * fix: 텍스트의 줄 바꿈이 제대로 되지 않는 오류 수정
    
    * reafctor: 네이밍 및 코드 컨벤션 통일
    
    * refactor: initialTime, initialEndTime 설정 로직을 함수로 분리
    
    * refactor: 예약 생성, 수정의 판단을 GuestReservation 컴포넌트에서 하도록 변경
    
    * refactor: 디렉토리 구조 정리
    
    * refactor: 오타 수정

--- frontend/src/pages/GuestReservation/GuestReservation.styles.ts
+++ frontend/src/pages/GuestReservation/GuestReservation.styles.ts
@@ -5,12 +5,16 @@ interface ColorDotProps {
   color: Color;
 }
 
-export const ReservationForm = styled.form`
-  margin: 1.5rem 0 5rem 0;
+export const Section = styled.section`
+  margin: 1.5rem 0 4.5rem;
 `;
 
-export const Section = styled.section`
-  margin: 1.5rem 0;
+export const Message = styled.p`
+  white-space: pre-wrap;
+`;
+
+export const ReservationList = styled.div`
+  border-top: 1px solid ${({ theme }) => theme.gray[400]};
 `;
 
 export const PageHeader = styled.h2`
@@ -20,39 +24,6 @@ export const PageHeader = styled.h2`
   display: flex;
   align-items: center;
 `;
-
-export const InputWrapper = styled.div`
-  position: relative;
-  display: flex;
-  gap: 1rem;
-  margin: 1.625rem 0;
-
-  label {
-    flex: 1;
-  }
-`;
-
-export const ReservationList = styled.div`
-  border-top: 1px solid ${({ theme }) => theme.gray[400]};
-`;
-
-export const ButtonWrapper = styled.div`
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  width: 100vw;
-`;
-
-export const TimeFormMessage = styled.p`
-  position: absolute;
-  left: 0.75rem;
-  bottom: -1rem;
-  font-size: 0.75rem;
-  height: 1em;
-  color: ${({ theme }) => theme.gray[500]};
-`;
-
-export const Message = styled.p``;
 
 export const ColorDot = styled.span<ColorDotProps>`
   display: inline-block;
