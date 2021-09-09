21a2156c53ea4d079b575e1b1032e31ffa6de783
Kimun Kim <kkm97351@gmail.com>
2021-09-10 02:40:28 +0900

    feat: MemberRepository에서 Member의 Id로 예약이 존재하는지 확인하는 기능 생성

--- backend/src/main/java/com/woowacourse/zzimkkong/repository/MemberRepository.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/repository/MemberRepository.java
@@ -2,6 +2,8 @@ package com.woowacourse.zzimkkong.repository;
 
 import com.woowacourse.zzimkkong.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -9,4 +11,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByEmail(String email);
 
     Optional<Member> findByEmail(String email);
+
+    @Query("SELECT COUNT(r) > 0 FROM Reservation r JOIN r.space s JOIN s.map m WHERE m.id = :memberId")
+    boolean existsReservationsByMemberId(@Param("memberId") Long memberId);
 }
