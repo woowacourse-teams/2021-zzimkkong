29e7d15d57c56bc042c23c465a26d79db530585d
yujo <yujo.dev@gmail.com>
2021-09-15 20:26:45 +0900

    refactor: managerSpaces 타입 가드 추가

--- frontend/src/api/managerSpaces.ts
+++ frontend/src/api/managerSpaces.ts
@@ -1,10 +1,11 @@
 import { AxiosResponse } from 'axios';
 import { QueryFunction, QueryKey } from 'react-query';
+import THROW_ERROR from 'constants/throwError';
 import { QueryManagerSpacesSuccess } from 'types/response';
 import api from './api';
 
 export interface QueryManagerSpacesParams {
-  mapId: number;
+  mapId: number | null;
 }
 
 export const queryManagerSpaces: QueryFunction<
@@ -13,6 +14,10 @@ export const queryManagerSpaces: QueryFunction<
 > = ({ queryKey }) => {
   const [, data] = queryKey;
   const { mapId } = data;
+
+  if (!mapId) {
+    throw new Error(THROW_ERROR.INVALID_MAP_ID);
+  }
 
   return api.get(`/managers/maps/${mapId}/spaces`);
 };
