db8fdb80331deaf430e737e89220bbc3f80f78fd
sakjung <ssangyu123@gmail.com>
2021-09-19 23:05:05 +0900

    test: asString 에 대한 테스트 추가

--- backend/src/test/java/com/woowacourse/zzimkkong/dto/EnabledDayOfWeekDtoTest.java
+++ backend/src/test/java/com/woowacourse/zzimkkong/dto/EnabledDayOfWeekDtoTest.java
@@ -11,7 +11,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class EnabledDayOfWeekDtoTest {
-
     @Test
     @DisplayName("들어온 day of week input에 해당하는 요일의 필드는 true로 설정한 뒤 응답 객체를 생성한다")
     void from() {
@@ -27,7 +26,9 @@ class EnabledDayOfWeekDtoTest {
     }
 
     @Test
+    @DisplayName("필드 중 true인 부분만 추출해서 ','으로 join한 string을 반환한다")
     void asString() {
-
+        final EnabledDayOfWeekDto enabledDayOfWeekDto = EnabledDayOfWeekDto.from("monday,tuesday,wednesday");
+        assertThat(enabledDayOfWeekDto.asString()).isEqualTo("monday,tuesday,wednesday");
     }
 }
