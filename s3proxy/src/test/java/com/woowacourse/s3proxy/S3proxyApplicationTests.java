6ddb6dec37b0b2ee3d2f636a988fe001c1c89845
Kimun Kim <kkm97351@gmail.com>
2021-09-19 22:42:17 +0900

    chore: s3 업로드 프록시 서버 프로젝트 생성

---
+++ s3proxy/src/test/java/com/woowacourse/s3proxy/S3proxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woowacourse.s3proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3proxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
