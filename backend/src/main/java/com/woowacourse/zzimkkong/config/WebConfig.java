aa5a325f4427381d43e277d183c3f91cae6a98a9
Kimun Kim <kkm97351@gmail.com>
2021-09-24 03:24:49 +0900

    refactor: Amazon Cloud 관련 의존성 및 객체 삭제

--- backend/src/main/java/com/woowacourse/zzimkkong/config/WebConfig.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/config/WebConfig.java
@@ -1,8 +1,8 @@
 package com.woowacourse.zzimkkong.config;
 
-import org.apache.http.HttpHeaders;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
