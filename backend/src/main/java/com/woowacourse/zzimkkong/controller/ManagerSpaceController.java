4dc0098baa9a2da8df27b646695d08c021a30245
xrabcde <xrabcde@gmail.com>
2021-09-26 18:42:20 +0900

    refactor: SpaceController에서 Member 사용하고 있는 부분 LoginEmail로 수정

--- backend/src/main/java/com/woowacourse/zzimkkong/controller/ManagerSpaceController.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/controller/ManagerSpaceController.java
@@ -1,8 +1,8 @@
 package com.woowacourse.zzimkkong.controller;
 
 import com.woowacourse.zzimkkong.domain.Manager;
-import com.woowacourse.zzimkkong.domain.Member;
 import com.woowacourse.zzimkkong.dto.space.*;
+import com.woowacourse.zzimkkong.infrastructure.LoginEmail;
 import com.woowacourse.zzimkkong.service.SpaceService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,16 +23,18 @@ public class ManagerSpaceController {
     public ResponseEntity<Void> save(
             @PathVariable final Long mapId,
             @RequestBody @Valid final SpaceCreateUpdateRequest spaceCreateRequest,
-            @Manager final Member member) {
-        SpaceCreateResponse spaceCreateResponse = spaceService.saveSpace(mapId, spaceCreateRequest, member);
+            @Manager final LoginEmail loginEmail) {
+        SpaceCreateResponse spaceCreateResponse = spaceService.saveSpace(mapId, spaceCreateRequest, loginEmail);
         return ResponseEntity
                 .created(URI.create("/api/managers/maps/" + mapId + "/spaces/" + spaceCreateResponse.getId()))
                 .build();
     }
 
     @GetMapping
-    public ResponseEntity<SpaceFindAllResponse> findAll(@PathVariable final Long mapId, @Manager final Member manager) {
-        SpaceFindAllResponse spaceFindAllResponse = spaceService.findAllSpace(mapId, manager);
+    public ResponseEntity<SpaceFindAllResponse> findAll(
+            @PathVariable final Long mapId,
+            @Manager final LoginEmail loginEmail) {
+        SpaceFindAllResponse spaceFindAllResponse = spaceService.findAllSpace(mapId, loginEmail);
         return ResponseEntity.ok().body(spaceFindAllResponse);
     }
 
@@ -40,8 +42,8 @@ public class ManagerSpaceController {
     public ResponseEntity<SpaceFindDetailResponse> find(
             @PathVariable final Long mapId,
             @PathVariable final Long spaceId,
-            @Manager final Member manager) {
-        SpaceFindDetailResponse spaceFindDetailResponse = spaceService.findSpace(mapId, spaceId, manager);
+            @Manager final LoginEmail loginEmail) {
+        SpaceFindDetailResponse spaceFindDetailResponse = spaceService.findSpace(mapId, spaceId, loginEmail);
         return ResponseEntity.ok().body(spaceFindDetailResponse);
     }
 
@@ -50,8 +52,8 @@ public class ManagerSpaceController {
             @PathVariable final Long mapId,
             @PathVariable final Long spaceId,
             @RequestBody final SpaceCreateUpdateRequest spaceCreateUpdateRequest,
-            @Manager final Member manager) {
-        spaceService.updateSpace(mapId, spaceId, spaceCreateUpdateRequest, manager);
+            @Manager final LoginEmail loginEmail) {
+        spaceService.updateSpace(mapId, spaceId, spaceCreateUpdateRequest, loginEmail);
         return ResponseEntity.ok().build();
     }
 
@@ -60,8 +62,8 @@ public class ManagerSpaceController {
             @PathVariable final Long mapId,
             @PathVariable final Long spaceId,
             @RequestBody SpaceDeleteRequest spaceDeleteRequest,
-            @Manager final Member manager) {
-        spaceService.deleteSpace(mapId, spaceId, spaceDeleteRequest, manager);
+            @Manager final LoginEmail loginEmail) {
+        spaceService.deleteSpace(mapId, spaceId, spaceDeleteRequest, loginEmail);
         return ResponseEntity.noContent().build();
     }
 }
