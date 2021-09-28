df0124553abbfac1207b4e3e1d1abe847ab5c378
y_woo :) <dusdn1702@naver.com>
2021-09-28 14:20:23 +0900

    refactor: 로그인 페이지 url 변경

--- backend/src/main/java/com/woowacourse/zzimkkong/controller/AdminController.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/controller/AdminController.java
@@ -24,7 +24,7 @@ public class AdminController {
         this.adminService = adminService;
     }
 
-    @GetMapping
+    @GetMapping("/login")
     public String loginPage() {
         return "login";
     }
