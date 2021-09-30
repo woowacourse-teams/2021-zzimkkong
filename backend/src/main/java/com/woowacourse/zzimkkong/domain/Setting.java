72c1a7bc704d960bc2253ef0abefe687b3fb5416
sakjung <ssangyu123@gmail.com>
2021-09-30 18:14:14 +0900

    chore: 불필요한 todo 지우기

--- backend/src/main/java/com/woowacourse/zzimkkong/domain/Setting.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/domain/Setting.java
@@ -36,7 +36,6 @@ public class Setting {
     @Column(nullable = false)
     private String enabledDayOfWeek;
 
-    // TODO: Build 하는 곳에서 EnabledDayOfWeek.toString() 해주기
     protected Setting(
             final LocalTime availableStartTime,
             final LocalTime availableEndTime,
