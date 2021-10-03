dfa82e89db8c2c9eb9083d07759e2152548f9734
Kimun Kim <kkm97351@gmail.com>
2021-10-04 04:39:33 +0900

    feat: 어노테이션을 통해 메소드의 실행 시간을 로깅하는 기능 구현

--- backend/src/main/java/com/woowacourse/zzimkkong/config/LogAspect.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/config/LogAspect.java
@@ -2,12 +2,14 @@ package com.woowacourse.zzimkkong.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
-import static net.logstash.logback.argument.StructuredArguments.value;
+import static net.logstash.logback.argument.StructuredArguments.*;
 
 @Slf4j
 @Component
@@ -20,5 +22,24 @@ public class LogAspect {
 
         log.info("API call: {}",
                 value("method", methodSignature.getDeclaringTypeName() + "." + methodSignature.getName() + "()"));
+    }
+
+    @Around("@target(com.woowacourse.zzimkkong.config.logaspect.LogMethodExecutionTime) " +
+            "&& execution(* com.woowacourse..*(..)))")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+
+        long startTime = System.currentTimeMillis();
+
+        Object result = joinPoint.proceed();
+
+        long endTime = System.currentTimeMillis();
+        long timeTaken = endTime - startTime;
+
+        log.info("API call: {} took {} ms",
+                value("method", methodSignature.getDeclaringTypeName() + "." + methodSignature.getName() + "()"),
+                value("execution_time", timeTaken));
+
+        return result;
     }
 }
