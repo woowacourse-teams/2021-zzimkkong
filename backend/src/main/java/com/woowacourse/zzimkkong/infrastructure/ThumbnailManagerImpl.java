a334d831245a219143143121392008fe1bd53caf
Kimun Kim <kkm97351@gmail.com>
2021-10-04 18:49:53 +0900

    refactor: 썸네일 업로드할 디렉토리 네임을 config를 통해 받음
    dev 서버의 이미지 유실을 방지합니다.

--- backend/src/main/java/com/woowacourse/zzimkkong/infrastructure/ThumbnailManagerImpl.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/infrastructure/ThumbnailManagerImpl.java
@@ -4,29 +4,36 @@ import com.woowacourse.zzimkkong.domain.Map;
 import com.woowacourse.zzimkkong.infrastructure.thumbnail.StorageUploader;
 import com.woowacourse.zzimkkong.infrastructure.thumbnail.SvgConverter;
 import com.woowacourse.zzimkkong.infrastructure.thumbnail.ThumbnailManager;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
 
 @Component
 public class ThumbnailManagerImpl implements ThumbnailManager {
-    public static final String THUMBNAILS_DIRECTORY_NAME = "thumbnails";
     public static final String THUMBNAIL_EXTENSION = ".png";
     private static final String THUMBNAIL_FILE_FORMAT = "%s";
 
     private final SvgConverter svgConverter;
     private final StorageUploader storageUploader;
+    private final String thumbnailsDirectoryName;
 
-    public ThumbnailManagerImpl(final SvgConverter svgConverter, final StorageUploader storageUploader) {
+    public ThumbnailManagerImpl(
+            final SvgConverter svgConverter,
+            final StorageUploader storageUploader,
+            @Value("${s3proxy.thumbnails-directory}")
+            final String thumbnailsDirectoryName) {
         this.svgConverter = svgConverter;
         this.storageUploader = storageUploader;
+        this.thumbnailsDirectoryName = thumbnailsDirectoryName;
+
     }
 
     public String uploadMapThumbnail(final String svgData, final Map map) {
         String fileName = makeThumbnailFileName(map);
         File pngFile = svgConverter.convertSvgToPngFile(svgData, fileName);
 
-        String thumbnailUrl = storageUploader.upload(THUMBNAILS_DIRECTORY_NAME, pngFile);
+        String thumbnailUrl = storageUploader.upload(thumbnailsDirectoryName, pngFile);
 
         pngFile.delete();
         return thumbnailUrl;
@@ -34,7 +41,7 @@ public class ThumbnailManagerImpl implements ThumbnailManager {
 
     public void deleteThumbnail(final Map map) {
         String fileName = makeThumbnailFileName(map);
-        storageUploader.delete(THUMBNAILS_DIRECTORY_NAME, fileName + THUMBNAIL_EXTENSION);
+        storageUploader.delete(thumbnailsDirectoryName, fileName + THUMBNAIL_EXTENSION);
     }
 
     private String makeThumbnailFileName(final Map map) {
