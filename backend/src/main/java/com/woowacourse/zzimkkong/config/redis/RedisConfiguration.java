db99fc05afa20c0dcdd81b3b916ba45c07b8ecd7
xrabcde <xrabcde@gmail.com>
2021-10-14 22:13:19 +0900

    fix: RedisConfiguration에 Profile 추가

--- backend/src/main/java/com/woowacourse/zzimkkong/config/redis/RedisConfiguration.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/config/redis/RedisConfiguration.java
@@ -3,6 +3,7 @@ package com.woowacourse.zzimkkong.config.redis;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -11,6 +12,7 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 
 @EnableRedisRepositories
 @Configuration
+@Profile("prod")
 public class RedisConfiguration {
     private final String host;
     private final String password;
