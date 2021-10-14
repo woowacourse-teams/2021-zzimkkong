815070c800cf58235c14d22417a0b9407bb04a11
xrabcde <xrabcde@gmail.com>
2021-10-14 18:25:18 +0900

    feat: RedisConfiguration 추가

---
+++ backend/src/main/java/com/woowacourse/zzimkkong/config/RedisConfiguration.java
@@ -0,0 +1,43 @@
+package com.woowacourse.zzimkkong.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@EnableRedisRepositories
+@Configuration
+public class RedisConfiguration {
+
+    private final String host;
+    private final String password;
+    private final int port;
+
+    public RedisConfiguration(
+            @Value("${spring.redis.host}") String host,
+            @Value("${spring.redis.password}") String password,
+            @Value("${spring.redis.port}") int port) {
+        this.host = host;
+        this.password = password;
+        this.port = port;
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration configuration =
+                new RedisStandaloneConfiguration(host, port);
+        configuration.setPassword(password);
+        return new LettuceConnectionFactory(configuration);
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
+}
