4a1cc0d1fbe0d12a9385c958b9019aa5708d69a3
Kimun Kim <kkm97351@gmail.com>
2021-10-17 01:51:23 +0900

    refactor: JpaRepository의 공통 메소드가 CrudRepository로 로깅되는 부분 해결

--- backend/src/main/java/com/woowacourse/zzimkkong/config/LogAspect.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/config/LogAspect.java
@@ -1,17 +1,27 @@
 package com.woowacourse.zzimkkong.config;
 
 import com.woowacourse.zzimkkong.config.logaspect.LogMethodExecutionTime;
+import com.woowacourse.zzimkkong.repository.MapRepository;
+import com.woowacourse.zzimkkong.repository.MemberRepository;
+import com.woowacourse.zzimkkong.repository.ReservationRepository;
+import com.woowacourse.zzimkkong.repository.SpaceRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
 import static net.logstash.logback.argument.StructuredArguments.value;
 
 @Slf4j
-@Component
+@Configuration
 @Aspect
 public class LogAspect {
     private static final String GROUP_NAME_OF_REPOSITORY = "repository";
@@ -25,7 +35,8 @@ public class LogAspect {
         long endTime = System.currentTimeMillis();
         long timeTaken = endTime - startTime;
 
-        String logGroup = getLogGroup(joinPoint);
+        String logGroup = getLogGroupFromAnnotation(joinPoint);
+
         MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
 
         logExecutionInfo(methodSignature, timeTaken, logGroup);
@@ -33,30 +44,78 @@ public class LogAspect {
         return result;
     }
 
-    @Around("execution(public * org.springframework.data.repository.Repository+.*(..))")
-    public Object logExecutionTimeOfRepository(ProceedingJoinPoint joinPoint) throws Throwable {
-
-        long startTime = System.currentTimeMillis();
-        Object result = joinPoint.proceed();
-        long endTime = System.currentTimeMillis();
-        long timeTaken = endTime - startTime;
-
-        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
-
-        logExecutionInfo(methodSignature, timeTaken, GROUP_NAME_OF_REPOSITORY);
-
-        return result;
-    }
-
-    private String getLogGroup(ProceedingJoinPoint joinPoint) {
+    private String getLogGroupFromAnnotation(ProceedingJoinPoint joinPoint) {
         Class<?> targetClass = joinPoint.getTarget().getClass();
         return targetClass.getAnnotation(LogMethodExecutionTime.class).group();
     }
 
-    private void logExecutionInfo(MethodSignature methodSignature, long timeTaken, String logGroup) {
+    private static void logExecutionInfo(MethodSignature methodSignature, long timeTaken, String logGroup) {
+        final Class<?> declaringType = methodSignature.getDeclaringType();
+        final Method method = methodSignature.getMethod();
+
+        logExecutionInfo(declaringType, method, timeTaken, logGroup);
+    }
+
+    private static void logExecutionInfo(Class<?> declaringType, Method method, long timeTaken, String logGroup) {
         log.info("{} took {} ms. (info group by '{}')",
-                value("method", methodSignature.getDeclaringTypeName() + "." + methodSignature.getName() + "()"),
+                value("method", declaringType.getName() + "." + method.getName() + "()"),
                 value("execution_time", timeTaken),
                 value("group", logGroup));
+    }
+
+    @Bean(name = "memberRepositoryLogProxy")
+    @Primary
+    public MemberRepository memberRepository(MemberRepository memberRepository) {
+        return createLogProxy(memberRepository, MemberRepository.class, GROUP_NAME_OF_REPOSITORY);
+    }
+
+    @Bean(name = "mapRepositoryLogProxy")
+    @Primary
+    public MapRepository mapRepository(MapRepository mapRepository) {
+        return createLogProxy(mapRepository, MapRepository.class, GROUP_NAME_OF_REPOSITORY);
+    }
+
+    @Bean(name = "spaceRepositoryLogProxy")
+    @Primary
+    public SpaceRepository spaceRepository(SpaceRepository spaceRepository) {
+        return createLogProxy(spaceRepository, SpaceRepository.class, GROUP_NAME_OF_REPOSITORY);
+    }
+
+    @Bean(name = "reservationRepositoryLogProxy")
+    @Primary
+    public ReservationRepository reservationRepository(ReservationRepository reservationRepository) {
+        return createLogProxy(reservationRepository, ReservationRepository.class, GROUP_NAME_OF_REPOSITORY);
+    }
+
+    private <T> T createLogProxy(Object target, Class<T> requiredType, String logGroup) {
+        final LogProxyHandler logProxyHandler = new LogProxyHandler(target, requiredType, logGroup);
+        return requiredType.cast(Proxy.newProxyInstance(
+                getClass().getClassLoader(),
+                new Class[]{requiredType},
+                logProxyHandler));
+    }
+
+    public static class LogProxyHandler implements InvocationHandler {
+        private final Object target;
+        private final Class<?> declaringType;
+        private final String logGroup;
+
+        public LogProxyHandler(Object target, Class<?> declaringType, String logGroup) {
+            this.target = target;
+            this.declaringType = declaringType;
+            this.logGroup = logGroup;
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            long startTime = System.currentTimeMillis();
+            final Object invokeResult = method.invoke(target, args);
+            long endTime = System.currentTimeMillis();
+            long timeTaken = endTime - startTime;
+
+            logExecutionInfo(declaringType, method, timeTaken, logGroup);
+
+            return invokeResult;
+        }
     }
 }
