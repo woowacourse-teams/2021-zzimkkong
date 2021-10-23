03b751aa006a8943421e2ecfb5a56cf769c2513e
Kimun Kim <kkm97351@gmail.com>
2021-10-24 06:30:27 +0900

    fix: AuthInterceptor 변경된 config에 맞도록 설정 변경

--- s3proxy/src/main/java/com/woowacourse/s3proxy/infrastructure/AuthInterceptor.java
+++ s3proxy/src/main/java/com/woowacourse/s3proxy/infrastructure/AuthInterceptor.java
@@ -1,22 +1,17 @@
 package com.woowacourse.s3proxy.infrastructure;
 
-import org.springframework.beans.factory.annotation.Value;
 import com.woowacourse.s3proxy.exception.AuthorizationHeaderUninvolvedException;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpMethod;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@Component
-@PropertySource("classpath:config/s3proxy.properties")
+
 public class AuthInterceptor implements HandlerInterceptor {
     private final String secretKey;
 
-    public AuthInterceptor(
-            @Value("${s3proxy.secret-key}") String secretKey) {
+    public AuthInterceptor(String secretKey) {
         this.secretKey = secretKey;
     }
 
