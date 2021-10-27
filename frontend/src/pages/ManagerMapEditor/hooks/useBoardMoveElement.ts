1f49c3ec970eef2f3d44f8bd7441915dbb255618
Shim MunSeong <Puterism.K@gmail.com>
2021-10-27 15:37:13 +0900

    refactor: `isNullish` 유틸 함수에 타입 가드 추가
    - `useBoardMoveElement`에 다시 적용

--- frontend/src/pages/ManagerMapEditor/hooks/useBoardMoveElement.ts
+++ frontend/src/pages/ManagerMapEditor/hooks/useBoardMoveElement.ts
@@ -1,6 +1,7 @@
 import { Dispatch, SetStateAction, useState } from 'react';
 import { Coordinate, MapElement } from 'types/common';
 import { MapElementType } from 'types/editor';
+import { isNullish } from 'utils/type';
 
 interface Props {
   coordinate: Coordinate;
@@ -61,7 +62,7 @@ const useBoardMoveElement = ({
         }
 
         if (element.type === MapElementType.Rect) {
-          if (element.x === undefined || element.y === undefined) {
+          if (isNullish(element.x) || isNullish(element.y)) {
             return element;
           }
 
@@ -96,7 +97,7 @@ const useBoardMoveElement = ({
         }
 
         if (element.type === MapElementType.Rect) {
-          if (element.x === undefined || element.y === undefined) {
+          if (isNullish(element.x) || isNullish(element.y)) {
             return element;
           }
 
