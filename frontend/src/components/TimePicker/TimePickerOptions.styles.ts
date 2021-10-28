4876c7857e9e3d7d7c0962ba952dd83f9bc1203b
Sunny K <sunnyk5780@gmail.com>
2021-10-28 18:49:45 +0900

    fix: TimePicker 라디오 기본 UI 가 나타나는 문제, 예약 버튼이 TimePicker 뒤에 위치하는 문제 (#723)
    * fix: TimePicker 라디오 기본 UI 가 나타나는 문제, 예약 버튼이 TimePicker 뒤에 위치하는 문제
    
    * fix: 모바일 사파리에서 라디오 기본 UI가 나타나는 문제

--- frontend/src/components/TimePicker/TimePickerOptions.styles.ts
+++ frontend/src/components/TimePicker/TimePickerOptions.styles.ts
@@ -69,9 +69,9 @@ export const Radio = styled.input`
   -webkit-appearance: none;
   -moz-appearance: none;
   appearance: none;
-  width: 1px;
-  height: 1px;
-  overflow: hidden;
+  opacity: 0;
+  width: 0;
+  height: 0;
 
   &:checked + ${OptionText} {
     color: ${({ theme }) => theme.gray[600]};
