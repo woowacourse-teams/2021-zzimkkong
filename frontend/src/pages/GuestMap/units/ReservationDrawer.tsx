851ab29f9f9e75d6ae5b6c0b0a4bc439908f4163
Shim MunSeong <Puterism.K@gmail.com>
2021-10-28 17:35:14 +0900

    fix: 모바일 크롬에서 예약자 예약 목록에서 예약 삭제 시, 예약 목록이 다시 fetch되지 않는 문제 수정
    - 예약 삭제 성공 후, `getReservations`를 refetch하도록 처리
    
    - `getReservations`를 refetch할 수 있도록, `ReservationDrawer` 유닛 컴포넌트에 정의된 `getReservations`를 페이지로 이동

--- frontend/src/pages/GuestMap/units/ReservationDrawer.tsx
+++ frontend/src/pages/GuestMap/units/ReservationDrawer.tsx
@@ -3,17 +3,19 @@ import { ReactComponent as EditIcon } from 'assets/svg/edit.svg';
 import Drawer from 'components/Drawer/Drawer';
 import IconButton from 'components/IconButton/IconButton';
 import ReservationListItem from 'components/ReservationListItem/ReservationListItem';
-import useGuestReservations from 'hooks/query/useGuestReservations';
 import { Reservation, Space } from 'types/common';
-import { formatDate, isPastDate, isPastTime } from 'utils/datetime';
+import { isPastDate, isPastTime } from 'utils/datetime';
 import { getReservationStatus } from 'utils/reservation';
 import * as Styled from './ReservationDrawer.styles';
 
 interface Props {
+  reservations: Reservation[];
   mapId: number;
   space: Space;
   date: Date;
   open: boolean;
+  isSuccess: boolean;
+  isLoadingError: boolean;
   onClose: () => void;
   onClickReservation: () => void;
   onEdit: (reservation: Reservation) => void;
@@ -21,94 +23,87 @@ interface Props {
 }
 
 const ReservationDrawer = ({
+  reservations,
   mapId,
   space,
   date,
   open,
+  isSuccess,
+  isLoadingError,
   onClose,
   onClickReservation,
   onEdit,
   onDelete,
-}: Props): JSX.Element => {
-  const getReservations = useGuestReservations({
-    mapId,
-    spaceId: space.id,
-    date: formatDate(date),
-  });
+}: Props): JSX.Element => (
+  <Drawer open={open} placement="bottom" onClose={onClose}>
+    <Styled.SpaceTitle>
+      <Styled.ColorDot color={space.color} />
+      {space.name}
+      {isPastDate(date) && (
+        <Styled.PastDateMessage>이전 날짜에는 예약할 수 없습니다.</Styled.PastDateMessage>
+      )}
+    </Styled.SpaceTitle>
+    <Styled.ReservationContainer>
+      {isLoadingError && (
+        <Styled.Message>
+          예약 목록을 불러오는 데 문제가 생겼어요!
+          <br />
+          새로 고침으로 다시 시도해주세요.
+        </Styled.Message>
+      )}
+      {isSuccess && reservations?.length === 0 && !isPastDate(date) && (
+        <Styled.Message>오늘의 첫 예약을 잡아보세요!</Styled.Message>
+      )}
+      {isSuccess && reservations?.length === 0 && isPastDate(date) && (
+        <Styled.Message>예약이 없습니다.</Styled.Message>
+      )}
 
-  const reservations = getReservations.data?.data?.reservations ?? [];
+      {isSuccess && reservations.length > 0 && (
+        <Styled.ReservationList role="list">
+          {reservations.map((reservation) => {
+            return (
+              <ReservationListItem
+                key={reservation.id}
+                data-testid={`reservation-${reservation.id}`}
+                reservation={reservation}
+                status={getReservationStatus(reservation.startDateTime, reservation.endDateTime)}
+                control={
+                  !isPastTime(new Date(reservation.endDateTime)) && (
+                    <Styled.IconButtonWrapper>
+                      <IconButton
+                        size="small"
+                        onClick={() => onEdit(reservation)}
+                        aria-label="수정"
+                      >
+                        <EditIcon width="100%" height="100%" />
+                      </IconButton>
+                      <IconButton
+                        size="small"
+                        onClick={() => onDelete(reservation)}
+                        aria-label="삭제"
+                      >
+                        <DeleteIcon width="100%" height="100%" />
+                      </IconButton>
+                    </Styled.IconButtonWrapper>
+                  )
+                }
+              />
+            );
+          })}
+        </Styled.ReservationList>
+      )}
+    </Styled.ReservationContainer>
 
-  return (
-    <Drawer open={open} placement="bottom" onClose={onClose}>
-      <Styled.SpaceTitle>
-        <Styled.ColorDot color={space.color} />
-        {space.name}
-        {isPastDate(date) && (
-          <Styled.PastDateMessage>이전 날짜에는 예약할 수 없습니다.</Styled.PastDateMessage>
-        )}
-      </Styled.SpaceTitle>
-      <Styled.ReservationContainer>
-        {getReservations.isLoadingError && (
-          <Styled.Message>
-            예약 목록을 불러오는 데 문제가 생겼어요!
-            <br />
-            새로 고침으로 다시 시도해주세요.
-          </Styled.Message>
-        )}
-        {getReservations.isSuccess && reservations?.length === 0 && !isPastDate(date) && (
-          <Styled.Message>오늘의 첫 예약을 잡아보세요!</Styled.Message>
-        )}
-        {getReservations.isSuccess && reservations?.length === 0 && isPastDate(date) && (
-          <Styled.Message>예약이 없습니다.</Styled.Message>
-        )}
-
-        {getReservations.isSuccess && reservations.length > 0 && (
-          <Styled.ReservationList role="list">
-            {reservations.map((reservation: Reservation) => {
-              return (
-                <ReservationListItem
-                  key={reservation.id}
-                  data-testid={`reservation-${reservation.id}`}
-                  reservation={reservation}
-                  status={getReservationStatus(reservation.startDateTime, reservation.endDateTime)}
-                  control={
-                    !isPastTime(new Date(reservation.endDateTime)) && (
-                      <Styled.IconButtonWrapper>
-                        <IconButton
-                          size="small"
-                          onClick={() => onEdit(reservation)}
-                          aria-label="수정"
-                        >
-                          <EditIcon width="100%" height="100%" />
-                        </IconButton>
-                        <IconButton
-                          size="small"
-                          onClick={() => onDelete(reservation)}
-                          aria-label="삭제"
-                        >
-                          <DeleteIcon width="100%" height="100%" />
-                        </IconButton>
-                      </Styled.IconButtonWrapper>
-                    )
-                  }
-                />
-              );
-            })}
-          </Styled.ReservationList>
-        )}
-      </Styled.ReservationContainer>
-
-      <Styled.ReservationButton
-        variant="primary"
-        size="large"
-        fullWidth
-        onClick={onClickReservation}
-        disabled={isPastDate(date)}
-      >
-        예약하기
-      </Styled.ReservationButton>
-    </Drawer>
-  );
-};
+    <Styled.ReservationButton
+      variant="primary"
+      size="large"
+      fullWidth
+      onClick={onClickReservation}
+      disabled={isPastDate(date)}
+    >
+      예약하기
+    </Styled.ReservationButton>
+  </Drawer>
+);
 
 export default ReservationDrawer;
