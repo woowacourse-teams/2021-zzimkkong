291c3f44e9ab8c0d93ebbb74db38640f5f585627
Kimun Kim <kkm97351@gmail.com>
2022-01-21 02:33:35 +0900

    refactor: S3Proxy 프로젝트 삭제

--- s3proxy/src/test/java/com/woowacourse/s3proxy/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.woowacourse.s3proxy;
-
-public class Constants {
-    public static final String LUTHER_IMAGE_URI_CLOUDFRONT = "https://d3tdpsdxqmqd52.cloudfront.net/testdir/luther.png";
-    public static final String LUTHER_IMAGE_URI_S3 = "https://zzimkkong-thumbnail-dev.s3.ap-northeast-2.amazonaws.com/testdir/luther.png";
-}
