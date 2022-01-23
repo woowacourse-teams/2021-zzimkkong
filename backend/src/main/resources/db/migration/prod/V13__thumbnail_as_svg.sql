0aa91ff6e8b7ec562fec3687bbb5443b9acf1324
Kimun Kim <kkm97351@gmail.com>
2022-01-23 23:25:09 +0900

    refactor: truncate 코드 추가

--- backend/src/main/resources/db/migration/prod/V13__thumbnail_as_svg.sql
+++ backend/src/main/resources/db/migration/prod/V13__thumbnail_as_svg.sql
@@ -1,1 +1,11 @@
 ALTER TABLE map CHANGE COLUMN map_image_url thumbnail longtext not null;
+
+SET foreign_key_checks = 0;
+
+TRUNCATE TABLE map;
+TRUNCATE TABLE member;
+TRUNCATE TABLE preset;
+TRUNCATE TABLE reservation;
+TRUNCATE TABLE space;
+
+SET foreign_key_checks = 1;
