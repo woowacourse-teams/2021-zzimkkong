18055a481c750dbaec28b468f8b26d657cab8de5
sakjung <ssangyu123@gmail.com>
2022-05-06 15:43:46 +0900

    feat: 공지 사항 100자 limit 검증 기능 추가

--- backend/src/main/java/com/woowacourse/zzimkkong/dto/map/NoticeCreateRequest.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/dto/map/NoticeCreateRequest.java
@@ -3,9 +3,14 @@ package com.woowacourse.zzimkkong.dto.map;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Size;
+
+import static com.woowacourse.zzimkkong.dto.ValidatorMessage.DESCRIPTION_MESSAGE;
+
 @Getter
 @NoArgsConstructor
 public class NoticeCreateRequest {
+    @Size(max = 100, message = DESCRIPTION_MESSAGE)
     private String notice;
 
     public NoticeCreateRequest(final String notice) {
