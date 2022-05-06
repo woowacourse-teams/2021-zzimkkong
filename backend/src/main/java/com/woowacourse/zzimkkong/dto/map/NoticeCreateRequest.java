70c83feeb269feba09822e6b94a8ab50e9facb8f
sakjung <ssangyu123@gmail.com>
2022-05-06 15:08:49 +0900

    chore: asciidoc api 추가

--- backend/src/main/java/com/woowacourse/zzimkkong/dto/map/NoticeCreateRequest.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/dto/map/NoticeCreateRequest.java
@@ -1,8 +1,14 @@
 package com.woowacourse.zzimkkong.dto.map;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class NoticeCreateRequest {
     private String notice;
+
+    public NoticeCreateRequest(final String notice) {
+        this.notice = notice;
+    }
 }
