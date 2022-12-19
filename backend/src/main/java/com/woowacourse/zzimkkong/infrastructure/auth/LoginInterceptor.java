064c0e34496391d33082035a09621ae5c1412b30
sakjung <ssangyu123@gmail.com>
2022-12-20 01:31:46 +0900

    feat: LoginInterceptor error handling 로직 추가

--- backend/src/main/java/com/woowacourse/zzimkkong/infrastructure/auth/LoginInterceptor.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/infrastructure/auth/LoginInterceptor.java
@@ -1,12 +1,14 @@
 package com.woowacourse.zzimkkong.infrastructure.auth;
 
 import com.woowacourse.zzimkkong.config.logaspect.LogMethodExecutionTime;
+import com.woowacourse.zzimkkong.exception.ZzimkkongException;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Component
 @LogMethodExecutionTime(group = "infrastructure")
@@ -18,13 +20,18 @@ public class LoginInterceptor implements HandlerInterceptor {
     }
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
         if (isPreflight(request)) {
             return true;
         }
 
-        String token = AuthorizationExtractor.extractAccessToken(request);
-        jwtUtils.validateToken(token);
+        try {
+            String token = AuthorizationExtractor.extractAccessToken(request);
+            jwtUtils.validateToken(token);
+        } catch (ZzimkkongException e) {
+            response.sendError(e.getStatus().value(), e.getMessage());
+            return false;
+        }
         return true;
     }
 
