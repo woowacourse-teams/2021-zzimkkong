4a8940b58f369b2ff011576f1afec3a2b152eae8
Shim MunSeong <Puterism.K@gmail.com>
2023-01-03 18:33:41 +0900

    refactor: 이메일 및 이름 중복 확인 API URL 변경 대응

--- frontend/src/api/join.ts
+++ frontend/src/api/join.ts
@@ -27,7 +27,7 @@ export const queryValidateEmail: QueryFunction = ({ queryKey }) => {
 
   if (typeof email !== 'string') throw new Error(THROW_ERROR.INVALID_EMAIL_FORMAT);
 
-  return api.get(`/members?email=${email}`);
+  return api.post(`/members/validations/email`, { email });
 };
 
 export const queryValidateUserName: QueryFunction = ({ queryKey }) => {
@@ -35,7 +35,7 @@ export const queryValidateUserName: QueryFunction = ({ queryKey }) => {
 
   if (typeof userName !== 'string') throw new Error(THROW_ERROR.INVALID_USER_NAME_FORMAT);
 
-  return api.get(`/members?userName=${userName}`);
+  return api.post(`/members/validations/username`, { userName });
 };
 
 export const postJoin = (params: JoinParams): Promise<AxiosResponse> => {
