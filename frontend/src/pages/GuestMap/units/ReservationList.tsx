8c02b130834c3441959e12ee20d8b595ddc883bf
조윤호 <yujo.dev@gmail.com>
2023-02-10 15:49:49 +0900

    feat: DatePicker 스타일 통일

--- frontend/src/pages/GuestMap/units/ReservationList.tsx
+++ frontend/src/pages/GuestMap/units/ReservationList.tsx
@@ -1,7 +1,12 @@
 import dayjs from 'dayjs';
 import { useState } from 'react';
-import DateInput from 'components/DateInput/DateInput';
+import { ReactComponent as CalendarIcon } from 'assets/svg/calendar.svg';
+import Input from 'components/Input/Input';
+import DATE from 'constants/date';
+import useGuestReservations from 'hooks/query/useGuestReservations';
 import { MapItem } from 'types/common';
+import { formatDate } from 'utils/datetime';
+import { isNullish } from 'utils/type';
 import * as Styled from './ReservationList.styled';
 
 interface Props {
@@ -10,13 +15,36 @@ interface Props {
 }
 
 const ReservationList = ({ map: { mapId }, selectedSpaceId }: Props) => {
-  const [date, setDate] = useState(dayjs());
+  const [date, setDate] = useState<string>(dayjs().format('YYYY-MM-DD'));
 
-  console.log('selectedSpaceId', selectedSpaceId);
+  const getReservations = useGuestReservations(
+    {
+      mapId: mapId,
+      spaceId: selectedSpaceId as number,
+      date: formatDate(dayjs(date)),
+    },
+    {
+      enabled: !isNullish(selectedSpaceId) && dayjs(date).isValid(),
+    }
+  );
+
+  const handleDate = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDate(e.target.value);
+  };
 
   return (
     <Styled.ReservationListWrapper>
-      <DateInput date={date} setDate={setDate} />
+      <Input
+        type="date"
+        name="date"
+        label="날짜"
+        icon={<CalendarIcon />}
+        value={date}
+        min={DATE.MIN_DATE_STRING}
+        max={DATE.MAX_DATE_STRING}
+        onChange={handleDate}
+        required
+      />
     </Styled.ReservationListWrapper>
   );
 };
