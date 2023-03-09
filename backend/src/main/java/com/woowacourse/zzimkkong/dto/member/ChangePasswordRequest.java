98cbefb7cf05dd7f07c3d8b986bb3b5e7b93dc9d
kimun kim <doublekim@woowahan.com>
2023-03-10 01:07:26 +0900

    refactor: ChangePasswordRequest dto final 속성 제거

--- backend/src/main/java/com/woowacourse/zzimkkong/dto/member/ChangePasswordRequest.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/dto/member/ChangePasswordRequest.java
@@ -1,6 +1,8 @@
 package com.woowacourse.zzimkkong.dto.member;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -8,18 +10,20 @@ import javax.validation.constraints.Pattern;
 import static com.woowacourse.zzimkkong.dto.ValidatorMessage.*;
 
 @Getter
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ChangePasswordRequest {
     @NotNull(message = EMPTY_MESSAGE)
     @Pattern(regexp = MEMBER_PW_FORMAT, message = MEMBER_PW_MESSAGE)
-    private final String oldPassword;
+    private String oldPassword;
 
     @NotNull(message = EMPTY_MESSAGE)
     @Pattern(regexp = MEMBER_PW_FORMAT, message = MEMBER_PW_MESSAGE)
-    private final String newPassword;
+    private String newPassword;
 
     @NotNull(message = EMPTY_MESSAGE)
     @Pattern(regexp = MEMBER_PW_FORMAT, message = MEMBER_PW_MESSAGE)
-    private final String newPasswordConfirm;
+    private String newPasswordConfirm;
 
     public ChangePasswordRequest(String oldPassword, String newPassword, String newPasswordConfirm) {
         this.oldPassword = oldPassword;
