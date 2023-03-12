ac6acd8ea134c2b83ac2277aa8b8432b54086fac
tributetothemoon <kkm97351@gmail.com>
2023-03-12 20:22:59 +0900

    test: ChangePasswordRequest dto 패턴 검사 테스트 작성

--- backend/src/test/java/com/woowacourse/zzimkkong/dto/ChangePasswordRequestTest.java
+++ backend/src/test/java/com/woowacourse/zzimkkong/dto/ChangePasswordRequestTest.java
@@ -3,8 +3,11 @@ package com.woowacourse.zzimkkong.dto;
 import com.woowacourse.zzimkkong.dto.member.ChangePasswordRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
 
+import static com.woowacourse.zzimkkong.dto.ValidatorMessage.EMPTY_MESSAGE;
 import static com.woowacourse.zzimkkong.dto.ValidatorMessage.MEMBER_PW_MESSAGE;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -40,5 +43,27 @@ class ChangePasswordRequestTest extends RequestTest {
         assertThat(getConstraintViolations(changePasswordRequest).stream()
                 .anyMatch(violation -> violation.getMessage().equals(MEMBER_PW_MESSAGE)))
                 .isTrue();
+    }
+
+    @ParameterizedTest
+    @NullSource
+    @DisplayName("비밀번호 변경시 새 비밀번호 란에 빈 문자열이 들어오면 처리한다.")
+    void blankPassword(String password) {
+        ChangePasswordRequest changePasswordRequest = new ChangePasswordRequest("oldPassword1234", password, password);
+
+        assertThat(getConstraintViolations(changePasswordRequest).stream()
+                .anyMatch(violation -> violation.getMessage().equals(EMPTY_MESSAGE)))
+                .isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"test1234!:false", "test1234:false", "1234test:false", "testtest:true", "12341234:true", "test123:true", "test1234test1234test1:true", "한글도실패1231:true"}, delimiter = ':')
+    @DisplayName("비밀번호 변경시 새 비밀번호 란에 옳지 않은 비밀번호 형식의 문자열이 들어오면 처리한다.")
+    void invalidPassword(String password, boolean flag) {
+        ChangePasswordRequest changePasswordRequest = new ChangePasswordRequest("oldPassword1234", password, password);
+
+        assertThat(getConstraintViolations(changePasswordRequest).stream()
+                .anyMatch(violation -> violation.getMessage().equals(MEMBER_PW_MESSAGE)))
+                .isEqualTo(flag);
     }
 }
