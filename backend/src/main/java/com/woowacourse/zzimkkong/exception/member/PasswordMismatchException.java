df8fd098294a132102f5f41d55ed2e854e5d78bc
Kimun Kim <kkm97351@gmail.com>
2023-03-12 20:29:37 +0900

    feat: 비밀번호 변경 api에서 기존 비밀번호 오기입시 에러 메세지 구체화, 기존 비밀번호에 대해 패턴 검사를 실시하지 않음, dto 패턴 테스트 코드 추가 (#929)
    * feat: 비밀번호 변경 요청에서 기존 비밀번호는 패턴을 검사하지 않음
    
    * feat: 비밀변호 변경 요청에서 기존 비밀번호 오기입시 예외 메세지 구체적으로 변경
    
    * test: ChangePasswordRequest dto 패턴 검사 테스트 작성

--- backend/src/main/java/com/woowacourse/zzimkkong/exception/member/PasswordMismatchException.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/exception/member/PasswordMismatchException.java
@@ -4,7 +4,7 @@ import com.woowacourse.zzimkkong.exception.InputFieldException;
 import org.springframework.http.HttpStatus;
 
 public class PasswordMismatchException extends InputFieldException {
-    private static final String MESSAGE = "비밀번호가 일치하지 않습니다.";
+    private static final String MESSAGE = "기존 비밀번호가 일치하지 않습니다.";
 
     public PasswordMismatchException() {
         super(MESSAGE, HttpStatus.BAD_REQUEST, PASSWORD);
