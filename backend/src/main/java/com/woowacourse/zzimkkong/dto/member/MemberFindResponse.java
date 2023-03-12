191f356f150460d29690868a993315019d85d53c
sakjung <ssangyu123@gmail.com>
2023-03-12 20:51:21 +0900

    feat: 내 정보 조회시 oauth provider 정보 필드 추가

--- backend/src/main/java/com/woowacourse/zzimkkong/dto/member/MemberFindResponse.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/dto/member/MemberFindResponse.java
@@ -1,6 +1,7 @@
 package com.woowacourse.zzimkkong.dto.member;
 
 import com.woowacourse.zzimkkong.domain.Member;
+import com.woowacourse.zzimkkong.domain.OauthProvider;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,18 +13,21 @@ public class MemberFindResponse {
     private String userName;
     private ProfileEmojiResponse emoji;
     private String organization;
+    private OauthProvider oauthProvider;
 
     private MemberFindResponse(
             final Long id,
             final String email,
             final String userName,
             final ProfileEmojiResponse profileEmojiResponse,
-            final String organization) {
+            final String organization,
+            final OauthProvider oauthProvider) {
         this.id = id;
         this.email = email;
         this.userName = userName;
         this.emoji = profileEmojiResponse;
         this.organization = organization;
+        this.oauthProvider = oauthProvider;
     }
 
     public static MemberFindResponse from(final Member member) {
@@ -32,6 +36,7 @@ public class MemberFindResponse {
                 member.getEmail(),
                 member.getUserName(),
                 ProfileEmojiResponse.from(member.getEmoji()),
-                member.getOrganization());
+                member.getOrganization(),
+                member.getOauthProvider());
     }
 }
