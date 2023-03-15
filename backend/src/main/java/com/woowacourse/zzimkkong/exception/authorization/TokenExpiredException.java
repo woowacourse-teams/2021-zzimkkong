178ad8d979a19fe26cba394fd868cfe7fa218c8d
sakjung <ssangyu123@gmail.com>
2023-03-15 10:28:18 +0900

    chore: token 만료시 나가는 메세지 변경

--- backend/src/main/java/com/woowacourse/zzimkkong/exception/authorization/TokenExpiredException.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/exception/authorization/TokenExpiredException.java
@@ -4,7 +4,7 @@ import com.woowacourse.zzimkkong.exception.ZzimkkongException;
 import org.springframework.http.HttpStatus;
 
 public class TokenExpiredException extends ZzimkkongException {
-    private static final String MESSAGE = "로그인이 필요한 서비스입니다.";
+    private static final String MESSAGE = "로그인 인증 유효기간이 만료되었습니다. 다시 로그인 해주세요.";
 
     public TokenExpiredException() {
         super(MESSAGE, HttpStatus.UNAUTHORIZED);
