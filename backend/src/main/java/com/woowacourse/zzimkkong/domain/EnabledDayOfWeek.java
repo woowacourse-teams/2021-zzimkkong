3f0c2053688b429f603ea674b1e9858324f4ef6e
sakjung <ssangyu123@gmail.com>
2023-04-03 00:47:47 +0900

    test: setting 요약 조회 API 인수 테스트 작성

--- backend/src/main/java/com/woowacourse/zzimkkong/domain/EnabledDayOfWeek.java
+++ backend/src/main/java/com/woowacourse/zzimkkong/domain/EnabledDayOfWeek.java
@@ -11,13 +11,13 @@ import java.util.stream.Collectors;
 @Slf4j
 @Getter
 public enum EnabledDayOfWeek {
-    MONDAY("월"),
-    TUESDAY("화"),
-    WEDNESDAY("수"),
-    THURSDAY("목"),
-    FRIDAY("금"),
-    SATURDAY("토"),
-    SUNDAY("일");
+    MONDAY("월요일"),
+    TUESDAY("화요일"),
+    WEDNESDAY("수요일"),
+    THURSDAY("목요일"),
+    FRIDAY("금요일"),
+    SATURDAY("토요일"),
+    SUNDAY("일요일");
 
     private final String displayName;
 
