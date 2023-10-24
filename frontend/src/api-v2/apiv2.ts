a50a57882b84270a64787351702e985ff7dab032
2yunseong <dbsdltjd3701@naver.com>
2023-10-24 18:53:22 +0900

    refactor: api v2 추상화로직 작성
    - mockoon의 port를 7742로 설정해야함.

---
+++ frontend/src/api-v2/apiv2.ts
@@ -0,0 +1,53 @@
+import axios, { AxiosError } from 'axios';
+import { history } from 'App';
+import PATH from 'constants/path';
+import { LOCAL_STORAGE_KEY } from 'constants/storage';
+import { ErrorResponse } from 'types/response';
+import { getLocalStorageItem, removeLocalStorageItem } from 'utils/localStorage';
+
+const apiV2 = axios.create({
+  baseURL: 'http://localhost:7742',
+  headers: {
+    'Content-type': 'application/json',
+  },
+});
+
+apiV2.interceptors.request.use(
+  (config) => {
+    const token = getLocalStorageItem({
+      key: LOCAL_STORAGE_KEY.ACCESS_TOKEN,
+      defaultValue: '',
+    });
+
+    if (typeof token !== 'string' || !token) return config;
+
+    config.headers = {
+      'Content-type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    };
+
+    return config;
+  },
+
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+apiV2.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+
+  (error: AxiosError<ErrorResponse>) => {
+    if (error?.response?.status === 401) {
+      removeLocalStorageItem({ key: LOCAL_STORAGE_KEY.ACCESS_TOKEN });
+
+      history.push(PATH.LOGIN);
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default apiV2;
