e86cd80c149811c39d3a5d6367599b684ce26394
2yunseong <dbsdltjd3701@naver.com>
2023-11-08 13:46:31 +0900

    feat: deleteReservation(예약 삭제) API 마이그레이션

--- frontend/src/api-v2/guestReservation.ts
+++ frontend/src/api-v2/guestReservation.ts
@@ -1,6 +1,12 @@
 import { AxiosResponse } from 'axios';
 import apiV2 from './apiv2';
 
+interface DeleteGuestReservationParamsV2 {
+  mapId: number;
+  spaceId: number;
+  reservationId: number;
+  password?: string;
+}
 export interface GuestReservationParamsV2 {
   reservation: {
     startDateTime: string;
@@ -35,4 +41,14 @@ export const putGuestReservationV2 = ({
   spaceId,
   reservationId,
 }: PutGuestReservationParamsV2): Promise<AxiosResponse<never>> =>
-  apiV2.put(`/guests/maps/${mapId}/spaces/${spaceId}/reservations/${reservationId}`, reservation);
+  apiV2.put(`/api/maps/${mapId}/spaces/${spaceId}/reservations/${reservationId}`, reservation);
+
+export const deleteGuestReservationV2 = ({
+  mapId,
+  spaceId,
+  reservationId,
+  password,
+}: DeleteGuestReservationParamsV2): Promise<AxiosResponse<never>> =>
+  apiV2.delete(`/api/maps/${mapId}/spaces/${spaceId}/reservations/${reservationId}`, {
+    data: { password },
+  });
